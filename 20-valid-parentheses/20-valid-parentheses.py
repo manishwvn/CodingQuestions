ea13a26d17a721fa9b52167c79bd5338343190ba
manishwvn <48592499+manishwvn@users.noreply.github.com>
2022-02-21 03:01:43 -0800

    Time: 55 ms (24.09%), Space: 13.9 MB (70.91%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        bracket_map = {"(": ")", "[": "]",  "{": "}"}
+        open_par = set(["(", "[", "{"])
+        stack = []
+        for i in s:
+            if i in open_par:
+                stack.append(i)
+            elif stack and i == bracket_map[stack[-1]]:
+                stack.pop()
+            else:
+                return False
+        return stack == []
+                
+        
