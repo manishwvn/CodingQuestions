cee5c315a13c2e244796366ca79b5aa3984d35cf
manishwvn <48592499+manishwvn@users.noreply.github.com>
2022-08-28 01:19:25 -0700

    Time: 36 ms (92.74%), Space: 13.9 MB (31.88%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,18 +1,5 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         
-        if len(s) == 0:
-            return t
-        
-        hm = collections.Counter(t)
-        for char in s:
-            if char in hm:
-                hm[char] -= 1
-                if hm[char] == 0:
-                    del hm[char]
-        
-        result = list(hm.keys())
-        return str(result[0])
-            
-            
+        return list(collections.Counter(t) - collections.Counter(s))[0]
         
