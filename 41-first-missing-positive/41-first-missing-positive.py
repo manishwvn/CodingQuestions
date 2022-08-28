a1b91505b0df288c952c175d7790248f19f132bc
manishwvn <48592499+manishwvn@users.noreply.github.com>
2022-08-28 02:19:17 -0700

    Time: 714 ms (89.03%), Space: 27.2 MB (91.39%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -3,15 +3,21 @@ class Solution:
         if len(nums) < 2 and nums[0] in {0, 1}:
             return nums[0] + 1
         
-        s = set(nums)
-        start = 1
-        
-        for _ in nums:
-            if start not in s:
-                return start
-            start += 1
+        i = 0
+        while i < len(nums):
+            correct = nums[i] - 1
             
-        return start
+            if nums[i] > 0 and nums[i] <= len(nums) and nums[i] != nums[correct]:
+                nums[i], nums[correct] = nums[correct], nums[i]
+                
+            else: 
+                i += 1
+        
+        print(nums)
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return i + 1
+        return len(nums) + 1
         
         
         
