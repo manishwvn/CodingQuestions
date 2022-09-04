a5a61c0536b6f6fc507738bf1ff68bbcee7a15d2
manishwvn <48592499+manishwvn@users.noreply.github.com>
2022-09-04 03:41:55 -0700

    Time: 58 ms (28.98%), Space: 13.9 MB (67.61%) - LeetHub

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.py
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.py
@@ -5,23 +5,20 @@ class Solution:
             return True if s[0] == '1' else 0
         
         curr_len_1, max_len_1 = 0, 0
+        curr_len_0, max_len_0 = 0, 0
+        
         for i in range(len(s)):
             if s[i] == '1':
                 curr_len_1 += 1
+                curr_len_0 = 0
             else:
                 curr_len_1 = 0
+                curr_len_0 += 1
                 
             max_len_1 = max(curr_len_1, max_len_1)
-            
-            
-        curr_len_0, max_len_0 = 0, 0
-        for i in range(len(s)):
-            if s[i] == '0':
-                curr_len_0 += 1
-            else:
-                curr_len_0 = 0
-                
             max_len_0 = max(curr_len_0, max_len_0)
-            
-        return True if max_len_1 > max_len_0 else False
         
+        return True if max_len_1 > max_len_0 else False
+            
+        
+    
