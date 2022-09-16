7517aa4a59ae9f0184a6510e9a2a15f5ab279424
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2022-09-16 02:47:46 -0700

    Time: 1127 ms (28.13%), Space: 14.3 MB (49.33%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.py
+++ 463-island-perimeter/463-island-perimeter.py
@@ -1,24 +1,34 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         
-        #bfs approach
-        
         m, n = len(grid), len(grid[0])
-        queue = deque()
         dirs = [[-1,0], [0,-1], [1,0],[0,1]]
         perimeter = 0
+        
+        def dfs(grid, i, j, m, n):
+            nonlocal perimeter        
+            if (i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 0):
+                perimeter += 1
+                return 
+            
+            
+            grid[i][j] = -1
+        
+            for dir in dirs:
+                nr, nc = i + dir[0], j + dir[1]
+                if nr < 0 or nr >= m:
+                    perimeter += 1
+                
+                if nc < 0 or nc >= n:
+                    perimeter += 1
+                
+                if nr>= 0 and nr < m and nc >=0 and nc < n and grid[nr][nc] == 0:
+                    dfs(grid, nr, nc, m, n)
+            
         
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    queue.append([i, j])
-                    grid[i][j] = -1
-                    
-        while queue:
-            r, c = queue.popleft()
-            for dir in dirs:
-                nr, nc = r + dir[0], c + dir[1]
-                if (nr < 0 or nr >= m or nc < 0 or nc >= n) or (nr>= 0 and nr <m and nc >=0 and nc <n and grid[nr][nc] == 0):
-                    perimeter += 1
+                    dfs(grid, i, j, m, n)
                     
         return perimeter
