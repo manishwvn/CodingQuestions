1250149126005b1a0988bc253473bdedaa720508
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2022-09-16 03:18:28 -0700

    Time: 474 ms (98.73%), Space: 14.3 MB (49.33%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.py
+++ 463-island-perimeter/463-island-perimeter.py
@@ -2,33 +2,19 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         
         m, n = len(grid), len(grid[0])
-        dirs = [[-1,0], [0,-1], [1,0],[0,1]]
         perimeter = 0
-        
-        def dfs(grid, i, j, m, n):
-            nonlocal perimeter        
-            if (i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 0):
-                perimeter += 1
-                return 
-            
-            
-            grid[i][j] = -1
-        
-            for dir in dirs:
-                nr, nc = i + dir[0], j + dir[1]
-                if nr < 0 or nr >= m:
-                    perimeter += 1
-                
-                if nc < 0 or nc >= n:
-                    perimeter += 1
-                
-                if nr>= 0 and nr < m and nc >=0 and nc < n and grid[nr][nc] == 0:
-                    dfs(grid, nr, nc, m, n)
-            
         
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    dfs(grid, i, j, m, n)
+                    perimeter += 4
                     
+                    if i > 0 and grid[i-1][j] == 1:
+                        perimeter -= 2
+                    
+                    
+                    if j > 0 and grid[i][j-1] == 1:
+                        perimeter -= 2
+                    
+        
         return perimeter
