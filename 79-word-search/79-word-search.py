ed01a7b8f121aa0da48f40301cec3fba3ecb4793
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2022-09-23 04:33:38 -0700

    Time: 9071 ms (18.97%), Space: 13.9 MB (92.98%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,37 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        m, n = len(board), len(board[0])
+        dirs = [[-1,0], [0,-1], [1,0],[0,1]]
+        
+        def backtrack(r, c, pos):
+            #base:
+            if pos == len(word):
+                return True
+            
+            if r < 0 or c < 0 or r == m or c == n or board[r][c] == "#":
+                return False
+            
+            #logic
+            if board[r][c] == word[pos]:
+                #action
+                char = board[r][c]
+                board[r][c] = "#"
+                
+                for dir in dirs:
+                    nr, nc = dir[0] + r, dir[1] + c
+                    if backtrack(nr, nc, pos + 1):
+                        return True
+                    
+                board[r][c] = char
+            
+            return False
+        
+        
+        
+        for i in range(m):
+            for j in range(n):
+                if backtrack(i, j, 0):
+                    return True
+        return False 
+        
