9e129ec4fd1d3ed9e067272df6d022516d2bd69a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2022-10-03 18:35:13 -0700

    Time: 79 ms (5.12%), Space: 13.9 MB (18.05%) - LeetHub

---
+++ 791-custom-sort-string/791-custom-sort-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        
+        counts = Counter(s)
+        res = ""
+        
+        print(counts)
+        for char in order:
+            if char in counts:
+                res = res + (counts[char] * char)
+                del counts[char]
+                
+        if counts:
+            for key, val in counts.items():
+                res = res + (key * val)
+                
+        return res
+                
+        
