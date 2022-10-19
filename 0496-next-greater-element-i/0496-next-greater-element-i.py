af30245abf7576609c2bfe371eb5297ca5460380
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2022-10-19 02:16:52 -0700

    Time: 95 ms (59.28%), Space: 14.2 MB (57.04%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,22 +1,19 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
-        hm = {n:i for i, n in enumerate(nums1)}
         res = [-1] * len(nums1)
+        hm = {n : i for i, n in enumerate(nums1)}
+        
+        stack = []
         
         for i in range(len(nums2)):
-            if nums2[i] not in hm:
-                continue
+            while stack and nums2[i] > stack[-1]:
+                value = stack.pop()
+                idx = hm[value]
+                res[idx] = nums2[i]
                 
-            for j in range(i+1, len(nums2)):
-                if nums2[j] > nums2[i]:
-                    idx = hm[nums2[i]]
-                    res[idx] = nums2[j]
-                    break
-                    
+            if nums2[i] in hm:
+                stack.append(nums2[i])
+            
         return res
-                    
-                    
-                
-                
         
