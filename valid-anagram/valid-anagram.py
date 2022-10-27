0447a908e884bc97b9ccc71a9dcfea77ce77a3b6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2022-10-26 17:35:11 -0700

    Time: 60 ms, Memory: 14.5 MB - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,3 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return collections.Counter(s)==collections.Counter(t)
+        if len(s) != len(t):
+            return False
+        
+        c1= collections.Counter(s)
+        
+        for char in t:
+            if char in c1:
+                c1[char] -= 1
+                if not c1[char]:
+                    del c1[char]
+        
+        return len(c1) == 0
+        
