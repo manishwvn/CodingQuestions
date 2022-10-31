e9cd8120821f05eccd95f3d16c10b9dc916afcf3
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2022-10-31 04:05:31 -0700

    Time: 117 ms (66.60%), Space: 13.9 MB (72.36%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,37 @@
+from copy import deepcopy
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        #for loop based recursion with backtracking
+        
+        """
+        n = number of elems
+        m = target
+        Time Complexity: O(2 ^ (m*n)) 
+        Space Complexity: O(2 ^ (m*n))
+        """
+        
+        if not candidates:
+            return []
+        
+        result = []
+        def helper(candidates, amount, path, pivot):
+            #base
+            if amount == 0:
+                result.append(path.copy())
+                return
+            
+            if amount < 0:
+                return 
+            
+            #logic
+            for i in range(pivot, len(candidates)):
+                path.append(candidates[i])
+                helper(candidates, amount - candidates[i], path, i)
+                path.pop()
+                
+        
+        helper(candidates, target, [], 0)
+        return result
+        
+        
