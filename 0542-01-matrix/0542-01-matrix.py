f68f3f8b9ff3ac3b9fc33a3412022b157a69e098
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2022-12-18 08:10:08 -0800

    Time: 1092 ms (68.28%), Space: 16.8 MB (80.96%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,31 +1,29 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         
+        dirs = [(0,1), (0,-1), (1,0), (-1,0)]
         m, n = len(mat), len(mat[0])
-        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        
+
         queue = deque()
-        
+
         for i in range(m):
             for j in range(n):
                 if mat[i][j] == 0:
-                    queue.append([i, j])
-                    
+                    queue.append((i,j))
                 else:
-                    mat[i][j] = -1
-                    
+                    mat[i][j] = float('inf')
+
         while queue:
             r, c = queue.popleft()
-            
-            for dir in dirs:
-                nr = r + dir[0]
-                nc = c + dir[1]
-                
-                if 0 <= nr < m and 0 <= nc < n and mat[nr][nc] == -1:
+
+            for dr, dc in dirs:
+                nr = r + dr
+                nc = c + dc
+
+                if 0 <= nr < m and 0 <= nc < n and mat[nr][nc] > mat[r][c] + 1:
                     mat[nr][nc] = mat[r][c] + 1
-                    
-                    queue.append([nr, nc])
-                    
+                    queue.append((nr,nc))
+
+
         return mat
-        
         
