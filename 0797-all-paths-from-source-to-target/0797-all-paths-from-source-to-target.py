2d38749ee23eb5c1202fd93e0beb06c139446aed
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2022-12-29 00:40:42 -0800

    Time: 114 ms (70.98%), Space: 16 MB (12.69%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,25 +1,51 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         
+#         n = len(graph)
+#         result = []
+        
+#         def dfs(src, path):
+            
+#             if src == n - 1 :
+#                 result.append(path.copy())
+#                 return
+            
+#             for neigh in graph[src]:
+#                 path.append(neigh)
+#                 dfs(neigh, path)
+#                 path.pop()
+                
+#         dfs(0, [0])
+#         return result
+
+
         n = len(graph)
+        queue = deque()
+        queue.append([0, [0]])
         result = []
         
-        def dfs(src, path):
+        while queue:
+            node, path = queue.popleft()
             
-            if src == n - 1 :
-                result.append(path)
-                return
-            
-            for neigh in graph[src]:
-                # path.append(neigh)
-                dfs(neigh, path + [neigh])
-                # path.pop(neigh)
-                
-                
-            # path.pop()
-                
-        dfs(0, [0])
+            if node == n - 1:
+                result.append(path.copy())
+                      
+            for nei in graph[node]:
+                # path.append(nei)
+                queue.append((nei, path + [nei]))
+                      
         return result
+                      
+                      
+        
+                
+                
+                
+            
+    
+    
+    
+    
         
         
         
