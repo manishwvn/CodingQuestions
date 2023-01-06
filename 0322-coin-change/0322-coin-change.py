db0823f955a41154ebac51bff9ab09245c2a2108
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-01-05 22:48:31 -0800

    Time: 5994 ms (5.63%), Space: 16.2 MB (26.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -5,30 +5,19 @@ class Solution:
             return 0
         
         n,m = len(coins), amount
-#         dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
         
-#         for j in range(1, m+1):
-#             dp[0][j] = m+1
-            
-#         for i in range(1, n + 1):
-#             for j in range(1, m + 1):
-#                 if j < coins[i-1]:
-#                     dp[i][j] = dp[i-1][j]
-                    
-#                 else:
-#                     dp[i][j] = min(dp[i-1][j], 1 + dp[i][j - coins[i-1]])
-        
-#         return -1 if dp[-1][-1] > amount else dp[-1][-1]
-
-        dp = [m+1 for _ in range(m+1)]
-        dp[0] = 0
-    
         for j in range(1, m+1):
-            for i in range(0, n):
-                if j >= coins[i]:
-                    dp[j] = min(dp[j], 1 + dp[j - coins[i]])
-                
-        return -1 if dp[-1] > amount else dp[-1]
-                
+            dp[0][j] = m+1
+            
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if j < coins[i-1]:
+                    dp[i][j] = dp[i-1][j]
+                    
+                else:
+                    dp[i][j] = min(dp[i-1][j], 1 + dp[i][j - coins[i-1]])
+        
+        return -1 if dp[-1][-1] > amount else dp[-1][-1]
                     
             
