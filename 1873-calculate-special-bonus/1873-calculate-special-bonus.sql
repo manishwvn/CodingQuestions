c1b2270cda7902d43750c1569c0e92b044028800
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-01-08 03:23:58 -0800

    Time: 1090 ms (20.83%), Space: 0B (100.00%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,12 @@
+SELECT 
+    EMPLOYEE_ID AS 'employee_id',
+    CASE
+        WHEN EMPLOYEE_ID % 2 <> 0 AND NAME NOT LIKE 'M%' THEN SALARY
+        ELSE 0
+    END AS 'bonus'
+FROM
+    EMPLOYEES
+ORDER BY
+    EMPLOYEE_ID;
+    
+
