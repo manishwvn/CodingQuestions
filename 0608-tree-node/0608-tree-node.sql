be9d3c94cb559a9a5eaa756eea325eaaa7fdd55b
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-01-08 03:59:58 -0800

    Time: 462 ms (71.61%), Space: 0B (100.00%) - LeetHub

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,11 @@
+-- USING CASE
+
+SELECT 
+    ID AS 'id',
+    CASE
+        WHEN P_ID IS NULL THEN 'Root'
+        WHEN ID IN (SELECT P_ID FROM TREE) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS 'Type'
+FROM
+    TREE;
