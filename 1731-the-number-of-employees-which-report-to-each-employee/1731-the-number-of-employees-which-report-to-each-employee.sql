199167137c4c5ab3b34c3b2a896318d2eea8b9fa
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-01-14 07:40:00 -0800

    Time: 714 ms (70.29%), Space: 0B (100.00%) - LeetHub

---
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,23 @@
+SELECT 
+    E1.EMPLOYEE_ID AS 'employee_id',
+    E1.NAME AS 'name', 
+    COUNT(E2.REPORTS_TO) AS 'reports_count', 
+    ROUND(AVG(E2.AGE)) AS 'average_age'
+    
+FROM 
+    EMPLOYEES E1
+    
+    INNER JOIN
+    
+    EMPLOYEES E2
+    
+    ON E1.EMPLOYEE_ID = E2.REPORTS_TO
+    
+GROUP BY 
+    E1.EMPLOYEE_ID
+
+ORDER BY
+    E1.EMPLOYEE_ID
+    
+    
+    
