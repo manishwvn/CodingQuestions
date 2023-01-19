7be77fbb5eec576448c4507225fe381f35d3ad3f
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-01-19 06:27:27 -0800

    Time: 1567 ms (7.54%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,23 +1,12 @@
 SELECT
     QUERY_NAME AS 'query_name',
     ROUND(AVG((RATING) / (POSITION)), 2) AS 'quality',
-    ROUND((((
-        COUNT(
-             CASE WHEN RATING < 3 
-             THEN RATING 
-             ELSE NULL 
-             END)) / 
-             (COUNT(RATING))) * 100), 2)
-    AS 'poor_query_percentage'
+    ROUND(AVG(RATING < 3)*100, 2) AS 'poor_query_percentage'
     
 FROM
     QUERIES
+    
 
 GROUP BY
     QUERY_NAME;
 
-
-# SELECT
-#     COUNT(CASE WHEN RATING < 3 THEN RATING ELSE NULL END)
-# FROM
-#     QUERIES;
