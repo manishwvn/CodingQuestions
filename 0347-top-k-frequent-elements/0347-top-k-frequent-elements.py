c567ffac819a62ce9c08a5c5ed2d4dcb8847827e
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-01-23 18:07:11 -0800

    Time: 106 ms (79.90%), Space: 18.6 MB (68.16%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,27 +2,20 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         
-        counts = Counter(nums)
-        buckets = {}
-        min_freq, max_freq = len(nums), 0
-
-        for num, count in counts.items():
-            if count in buckets:
-                buckets[count].append(num)
-            else:
-                buckets[count] = [num]
-
-            min_freq = min(min_freq, count)
-            max_freq = max(max_freq, count)
-
-        result = []
-
-        for i in range(max_freq, min_freq-1, -1):
-            if i in buckets:
-                for num in buckets[i]:
-                    result.append(num)
-                    if len(result) == k:
-                        return result
-
-        return []
-
+        nums = Counter(nums)
+        heap = []
+        res = []
+        
+        for num, count in nums.items():
+            heappush(heap, (-count, num))
+            
+        while k:
+            count, num = heappop(heap)
+            res.append(num)
+            k -= 1
+                
+        return res
+            
+            
+            
+        
