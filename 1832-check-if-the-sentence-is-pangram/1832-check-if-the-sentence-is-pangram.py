92bcabf8a744c14884881c41bb00cdca14d80db6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-01-25 18:48:38 -0800

    Time: 41 ms (40.81%), Space: 13.9 MB (8.76%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,8 +1,4 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
         
-        lookup = set(chr(i) for i in range(97, 123))
-        charSet = set(char for char in sentence)
-        
-        return charSet == lookup
-        
+        return len(set(sentence)) == 26
