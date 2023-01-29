cb16c2a8e8179d0fcebe2d84762deab6073fedfd
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-01-29 01:13:10 -0800

    Time: 1346 ms (7.36%), Space: 0B (100.00%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,5 +1,14 @@
 SELECT
-    S.SCORE AS score,
-    DENSE_RANK() OVER (ORDER BY S.SCORE DESC) AS 'rank'
+    S.SCORE, 
+    COUNT(DISTINCT T.SCORE) AS 'rank'
 FROM
     SCORES S
+JOIN
+    SCORES T
+ON 
+    S.SCORE <= T.SCORE
+GROUP BY
+    S.ID,
+    S.SCORE
+ORDER BY
+    1 DESC;
