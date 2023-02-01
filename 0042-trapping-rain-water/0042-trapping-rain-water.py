203e1ce95cc1e9ee82aacf8a0dc1d8e1fd400fff
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-01-31 23:04:32 -0800

    Time: 165 ms (37.93%), Space: 16.2 MB (9.85%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,25 +4,34 @@ class Solution:
         if not height:
             return 0
 
-        l, r = 0, len(height) - 1
-        leftMax, rightMax = height[l], height[r]
-        res = 0
-        while l < r:
-            if leftMax < rightMax:
-                l += 1
-                leftMax = max(leftMax, height[l])
-                res += leftMax - height[l]
-            else:
-                r -= 1
-                rightMax = max(rightMax, height[r])
-                res += rightMax - height[r]
-        return res
-                
-                
+        result = 0
+        n = len(height)
+        
+        dp_left, dp_right = [0] * n, [0] * n
+        
+        dp_left[0], dp_right[n - 1] = height[0], height[n-1]
+        
+        for i in range(1, n):
+            dp_left[i] = max(height[i], dp_left[i - 1])
             
-                
-                
+        print(dp_left)
+        
+        
+        for i in range(n-2, -1, -1):
+            dp_right[i] = max(height[i], dp_right[i+1])
             
+        print(dp_right)
+        
+        
+        for i in range(n):
+            result += (min(dp_left[i], dp_right[i]) - height[i])
+            
+        return result
+        
+        
+        
+            
+        
         
         
         
