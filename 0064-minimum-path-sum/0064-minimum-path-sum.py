a169922395bfbc0ab883d2d379ef318eef290462
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-01-31 18:26:45 -0800

    Time: 109 ms (58.05%), Space: 15.9 MB (30.71%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -3,21 +3,17 @@ class Solution:
         
         m, n = len(grid), len(grid[0])
         
-        dp = [[0 for _ in range(n)] for _ in range(m)]
-        
-        for r in range(m-1, -1, -1):
-            for c in range(n-1, -1, -1):
-                
-                if r == m - 1 and c != n - 1:
-                    dp[r][c] = grid[r][c] + dp[r][c+1]
-                    
-                elif c == n - 1 and r != m - 1:
-                    dp[r][c] = grid[r][c] + dp[r+1][c]
-                    
-                elif r != m - 1 and c != n - 1:
-                    dp[r][c] = min(dp[r+1][c], dp[r][c+1]) + grid[r][c]
-                else:
-                    dp[r][c] = grid[r][c]
-                    
-        return dp[0][0]
+        for r in range(0, m):
+            for c in range(0, n):
+
+                if r == 0 and c != 0:
+                    grid[r][c] = grid[r][c] + grid[r][c-1]
+
+                elif c == 0 and r != 0:
+                    grid[r][c] = grid[r][c] + grid[r-1][c]
+
+                elif r != 0 and c != 0:
+                    grid[r][c] = min(grid[r-1][c], grid[r][c-1]) + grid[r][c]
+
+        return grid[m-1][n-1]
         
