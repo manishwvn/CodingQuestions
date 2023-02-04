c0dd2103c0a9ca293a2c75b48a4968d4fc4833e9
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-02-04 06:53:11 -0800

    Time: 170 ms (92.71%), Space: 23.8 MB (41.54%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -12,11 +12,11 @@ class Solution:
                 if nums[mid] == nums[mid + 1]:
                     left = mid + 2
                 else:
-                    right = mid
+                    right = mid - 1
             else:
                 if nums[mid] == nums[mid - 1]:
                     left = mid + 1
                 else:
-                    right = mid - 1
+                    right = mid - 2
 
         return nums[left]
