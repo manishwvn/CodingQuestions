45ea62127f83e5b22424a0faae1b9970874c2f80
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-02-04 04:23:20 -0800

    Time: 32 ms (71.54%), Space: 13.9 MB (56.19%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        
+        if len(s) != len(goal):
+            return False
+        
+        return goal in s + s
+        
+        
+        
