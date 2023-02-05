210ac66ca7960b89db22876e7ee05cb526cbec3a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-02-04 17:26:28 -0800

    Time: 631 ms (22.02%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,14 +1,29 @@
-with CTE as (
-    select student, 
-case 
+# with CTE as (
+#     select student, 
+# case 
+#     when id%2 = 0 then id-1
+#     when id%2 = 1 and id != (select count(*) from seat) then id+1
+#     else id end 
+# 	as id
+    
+#     from seat
+# )
+
+# select id,  student
+# from CTE
+# order by id
+
+
+select
+    case 
     when id%2 = 0 then id-1
     when id%2 = 1 and id != (select count(*) from seat) then id+1
     else id end 
-	as id
+	as id,
     
-    from seat
-)
+    student
 
-select id,  student
-from CTE
-order by id
+from    
+    seat
+order by id;
+    
