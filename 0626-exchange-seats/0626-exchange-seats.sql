59daa57a402e3b6f41e104881a50fee4cf795f74
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-02-04 17:25:35 -0800

    Time: 599 ms (27.46%), Space: 0B (100.00%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,14 @@
+with CTE as (
+    select student, 
+case 
+    when id%2 = 0 then id-1
+    when id%2 = 1 and id != (select count(*) from seat) then id+1
+    else id end 
+	as id
+    
+    from seat
+)
+
+select id,  student
+from CTE
+order by id
