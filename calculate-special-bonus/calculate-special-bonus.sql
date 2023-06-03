20b569e729e428b3fcc5596e302e67200589774f
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-06-02 20:10:21 -0700

    Time: 1688 ms, Memory: 0B - LeetHub

--- calculate-special-bonus/calculate-special-bonus.sql
+++ calculate-special-bonus/calculate-special-bonus.sql
@@ -1,12 +1,20 @@
-select
-    employee_id,
-        case
-            when employee_id % 2 <> 0 and name not like 'M%' then salary
-            else 0
-        end
-    as bonus
-from
-    employees
-order by
-    employee_id;
+# select
+#     employee_id,
+#         case
+#             when employee_id % 2 <> 0 and name not like 'M%' then salary
+#             else 0
+#         end
+#     as bonus
+# from
+#     employees
+# order by
+#     employee_id;
         
+        
+select
+    employee_id, 
+    salary * (employee_id % 2) * (name not like 'M%') as bonus
+from 
+    employees
+order by 
+    employee_id;
