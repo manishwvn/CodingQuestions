e9f5e28cd41fca63ff98d91968793f7924ba644a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2023-06-02 20:02:59 -0700

    Time: 1402 ms, Memory: 0B - LeetHub

--- calculate-special-bonus/calculate-special-bonus.sql
+++ calculate-special-bonus/calculate-special-bonus.sql
@@ -1,7 +1,7 @@
 select
     employee_id,
         case
-            when employee_id % 2 <> 0 and substring(name, 1, 1) not like 'M' then salary
+            when employee_id % 2 <> 0 and name not like 'M%' then salary
             else 0
         end
     as bonus
