314036bf1492dfc081cf9aa89668baf0565fe4e5
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-06-02 21:03:34 -0700

    Time: 484 ms (92.74%) | Memory: 17.8 MB (99.27%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -9,7 +9,8 @@ class Solution:
         for i in range(len(arr)-2, -1, -1):
             val = arr[i]
             arr[i] = max_val
-            max_val = max(val, max_val)
+            # max_val = max(val, max_val)
+            if val > max_val: max_val = val
 
         arr[-1] = -1
         return arr 
