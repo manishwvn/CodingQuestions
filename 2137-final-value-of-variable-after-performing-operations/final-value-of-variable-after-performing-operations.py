d169e8190204a80f35f54be92815d63b01f183a0
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-06-07 11:58:47 -0700

    Time: 43 ms (97.78%) | Memory: 16.5 MB (87.90%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
@@ -3,8 +3,8 @@ class Solution:
 
         x = 0
         for op in operations:
-            if '+' in op: x += 1
-            if '-' in op: x -= 1
+            if op[1] == '+': x += 1
+            else: x -= 1
 
         return x
         
