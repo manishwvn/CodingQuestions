674ea6a3a46a4daff6e9fcdf3e817e0c620ef5b4
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-06-09 23:56:52 -0700

    Time: 645 ms (97.42%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,11 +1,9 @@
-WITH RankedPersons AS (
-    SELECT id, email,
-           ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS row_num
-    FROM Person
-)
 DELETE FROM Person
 WHERE id IN (
     SELECT id
-    FROM RankedPersons
+    FROM (
+        SELECT id, ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS row_num
+        FROM Person
+    )
     WHERE row_num > 1
 );
