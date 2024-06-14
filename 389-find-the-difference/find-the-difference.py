6f5380a891dec1c5dfea01315203e22d140cfddd
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-06-14 12:01:26 -0700

    Time: 42 ms (30.00%) | Memory: 16.5 MB (71.64%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,10 +1,10 @@
-from collections import Counter
-
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        counts = Counter(s)
-        for ch in t:
-            if ch not in counts or counts[ch] == 0:
-                return ch
-            else:
-                counts[ch] -= 1
+        ch = 0
+        for char in s:
+            ch ^= ord(char)
+
+        for char in t:
+            ch ^= ord(char)
+
+        return chr(ch)
