133fca27392c095c888ca9fc9fb752bb2ab55487
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-06-19 01:04:07 -0700

    Time: 193 ms (97.41%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,17 @@
+select
+    m1.machine_id,
+    abs(round(avg(m1.timestamp - m2.timestamp), 3)) as processing_time
+from
+    activity m1
+join
+    activity m2
+on
+    m1.machine_id = m2.machine_id
+    and
+    m1.process_id = m2.process_id
+    and
+    m1.activity_type = 'start' and m2.activity_type = 'end'
+group by
+    1;
+
+    
