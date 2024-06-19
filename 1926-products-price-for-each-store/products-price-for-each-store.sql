432f3a9d26dce21048cb4759107da8b4844447ef
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-06-19 01:28:55 -0700

    Time: 625 ms (75.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1926-products-price-for-each-store/products-price-for-each-store.sql
@@ -0,0 +1,8 @@
+select 
+    product_id,
+    max(case when store = 'store1' then price else null end) as 'store1',
+    max(case when store = 'store2' then price else null end) as 'store2',
+    max(case when store = 'store3' then price else null end) as 'store3'
+from
+    products
+group by 1;
