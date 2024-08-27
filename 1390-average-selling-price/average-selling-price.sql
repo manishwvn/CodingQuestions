a918756cb7cfe12c25efc0f214c7deffce5db19c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-08-26 21:58:18 -0700

    Time: 212 ms (68.95%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,7 +1,6 @@
-# Write your MySQL query statement below
-SELECT p.product_id, IFNULL(round(SUM(p.price*u.units)/sum(u.units),2),0) as average_price
-FROM Prices p 
+-- Write your PostgreSQL query statement below
+SELECT p.product_id, COALESCE(ROUND(SUM(u.units*p.price)/SUM(u.units)::decimal, 2), 0)average_price
+FROM Prices p
 LEFT JOIN UnitsSold u
-ON p.product_id = u.product_id AND 
-u.purchase_date BETWEEN p.Start_date and p.end_date
+ON u.product_id = p.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id
