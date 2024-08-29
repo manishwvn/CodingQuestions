3de1365b11bfdb21f72b7bdde6c1d97582b4ec18
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-08-29 14:54:08 -0700

    Time: 258 ms (36.57%) | Memory: 0B (100.00%) - LeetSync

--- 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
+++ 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
@@ -1,22 +1,8 @@
-WITH total_friend_requests AS (
-    SELECT COUNT(*) AS total_count 
-    FROM (
-        SELECT DISTINCT sender_id, send_to_id 
-        FROM FriendRequest
-    ) AS distinct_requests
-),
-total_accepted_requests AS (
-    SELECT COUNT(*) AS accept_count 
-    FROM (
-        SELECT DISTINCT requester_id, accepter_id 
-        FROM RequestAccepted
-    ) AS distinct_acceptances
-)
 SELECT 
     CASE 
-        WHEN (SELECT total_count FROM total_friend_requests) = 0 THEN 0.00
+        WHEN (SELECT COUNT(*) FROM (SELECT DISTINCT sender_id, send_to_id FROM FriendRequest) AS req) = 0 THEN 0.00
         ELSE ROUND(
-            (SELECT accept_count FROM total_accepted_requests)::DECIMAL /
-            (SELECT total_count FROM total_friend_requests), 2
+            (SELECT COUNT(*) FROM (SELECT DISTINCT requester_id, accepter_id FROM RequestAccepted) AS acc)::DECIMAL /
+            (SELECT COUNT(*) FROM (SELECT DISTINCT sender_id, send_to_id FROM FriendRequest) AS req), 2
         )
     END AS accept_rate;
