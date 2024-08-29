cf48b3e336f9995f1c8986a98daaaa1c838e9ba4
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-08-29 16:22:59 -0700

    Time: 331 ms (19.02%) | Memory: 0B (100.00%) - LeetSync

--- 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
+++ 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
@@ -1,15 +1,21 @@
-select
-    question_id as survey_log
-from
-    SurveyLog
-group by 
-    question_id
-order by
-    coalesce(sum(
-        case when action = 'answer' then 1 else 0 end
-    )::decimal / 
-    nullif(sum(
-        case when action = 'show' then 1 else 0 end
-    ),0), 0) desc,
-    question_id asc
-limit 1;
+WITH AnswerRates AS (
+    SELECT
+        question_id,
+        COALESCE(
+            SUM(CASE WHEN action = 'answer' THEN 1 ELSE 0 END)::decimal /
+            NULLIF(SUM(CASE WHEN action = 'show' THEN 1 ELSE 0 END), 0),
+            0
+        ) AS answer_rate
+    FROM
+        SurveyLog
+    GROUP BY
+        question_id
+)
+SELECT
+    question_id AS survey_log
+FROM
+    AnswerRates
+ORDER BY
+    answer_rate DESC,
+    question_id ASC
+LIMIT 1;
