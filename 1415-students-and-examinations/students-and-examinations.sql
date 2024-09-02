615916a5f78c9658d99a55edfafcdc2db8dbce46
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-09-01 22:31:42 -0700

    Time: 386 ms (86.09%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,13 +1,18 @@
-SELECT 
-s.student_id, 
-s.student_name, 
-sub.subject_name, 
-COUNT(e.student_id) AS attended_exams
+WITH 
+students_with_subj AS (
+SELECT DISTINCT s.student_id,
+       s.student_name,
+       Subjects.subject_name 
 FROM Students AS s
-CROSS JOIN Subjects AS sub
-LEFT JOIN 
-Examinations AS e 
-ON s.student_id = e.student_id 
-AND sub.subject_name = e.subject_name
-GROUP BY s.student_id, s.student_name, sub.subject_name
-ORDER BY s.student_id, sub.subject_name;
+CROSS JOIN Subjects)
+
+
+SELECT s.student_id,
+       s.student_name,
+       s.subject_name,
+       COUNT(e.subject_name) AS attended_exams
+FROM students_with_subj AS s
+LEFT JOIN Examinations AS e ON e.student_id = s.student_id
+                           AND e.subject_name = s.subject_name
+GROUP BY s.student_id, s.student_name, s.subject_name
+ORDER BY s.student_id, s.subject_name
