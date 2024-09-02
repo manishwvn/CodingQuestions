ccbe41b64dac3f7f2010eaafdfc4052533a20537
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-09-01 21:33:39 -0700

    Time: 740 ms (21.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,25 @@
+WITH FirstLogin AS (
+    SELECT
+        player_id,
+        MIN(event_date) AS first_login_date
+    FROM
+        activity
+    GROUP BY
+        player_id
+),
+RecurringPlayers AS (
+    SELECT
+        fl.player_id
+    FROM
+        FirstLogin fl
+    JOIN
+        activity a
+    ON
+        fl.player_id = a.player_id
+    AND
+        a.event_date = fl.first_login_date + INTERVAL '1 day'
+)
+SELECT
+    ROUND(COUNT(rp.player_id)::numeric / (SELECT COUNT(DISTINCT player_id) FROM activity)::numeric, 2) AS fraction
+FROM
+    RecurringPlayers rp;
