aecdcd25ba452f49910d2b4164ccd0e66d288541
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-10-03 17:00:00 -0700

    Time: 88 ms (65.28%) | Memory: 22.1 MB (24.36%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        result = {}
+
+        for str in strs:
+            counts = [0] * 26
+
+            for char in str:
+                counts[ord(char) - ord('a')] += 1
+
+            if tuple(counts) in result:
+                result[tuple(counts)].append(str)
+            else:
+                result[tuple(counts)] = [str]
+
+        return result.values()
