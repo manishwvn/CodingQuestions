61a8a92af35b73dfd14dbfc329490b3e0b39c694
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-10-08 03:15:16 -0700

    Time: 44 ms (8.65%) | Memory: 16.6 MB (38.94%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -8,14 +8,9 @@ class Solution:
             temp = [1]
 
             for j in range(1, i):
-                
-                if i == 1 and i == n:
-                    temp.append(result[i][j])
-
-                else:
-                    temp.append(result[i-1][j-1] + result[i-1][j])
+                temp.append(result[i-1][j-1] + result[i-1][j])
                     
-            temp.append(1)
+            temp.append(1)  # Add the last 1 to the row
             result.append(temp)
 
         return result
