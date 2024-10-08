ee34e12a9ab2ae1dc7d654f3562cca4e8e30c995
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-10-08 03:16:13 -0700

    Time: 37 ms (51.81%) | Memory: 16.6 MB (38.94%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,16 +1,14 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
 
-        result = [[1]]
-        if numRows == 1: return result
+        result = [[1]]  
 
         for i in range(1, numRows):
-            temp = [1]
+            temp = [1] * (i + 1)
 
             for j in range(1, i):
-                temp.append(result[i-1][j-1] + result[i-1][j])
-                    
-            temp.append(1)  # Add the last 1 to the row
+                temp[j] = result[i-1][j-1] + result[i-1][j]
+            
             result.append(temp)
 
         return result
