015de4aad93a2c05c6a56dc3889473ee6ea6ea3f
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-10-07 21:35:03 -0700

    Time: 47 ms (5.59%) | Memory: 16.7 MB (37.85%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,6 +1,8 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
 
+        # abbc, abb, abcd
+
         result = list(strs[0])            
         
         for i in range(1, len(strs)):
@@ -9,11 +11,12 @@ class Solution:
 
             for j in range(len(strs[i])):
 
-                if j >= len(result):  # 0, 2
-                    break
-                
-                elif len(result) > len(strs[i]):
+                # if j >= len(result):  # abcd , ab
+                #     break
+                if j < len(result) or len(result) > len(strs[i]):
                     result = result[:len(strs[i])]
+                else:
+                    break     
                 
                 if result[j] != strs[i][j]: # a,a 
                     result = result[:j]
