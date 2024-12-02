3298ed07e96874def51a7caae5eaf72fb5e6d164
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-02 15:40:10 -0800

    Time: 563 ms (36.05%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -6,16 +6,10 @@ with order_counts as (
         orders
     group by
         customer_number
-),
-max_order_count as (
-    select
-        max(orders) as max_orders
-    from
-        order_counts
 )
 select
     customer_number
 from
     order_counts
 where
-    orders = (select max_orders from max_order_count);
+    orders = (select max(orders) from order_counts);
