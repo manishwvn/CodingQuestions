b09870c4d9258c42560c98d5b60ac2916531a44c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-02 15:24:44 -0800

    Time: 625 ms (44.62%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,10 +1,10 @@
 select
-    c.name as customers
+    c.name as Customers
 from
     Customers c
 left join
-    Orders o
+    orders o
 on
     c.id = o.customerid
 where
-    o.customerid is NULL;
+    o.id is null;
