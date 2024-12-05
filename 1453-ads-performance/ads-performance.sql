0afc9df2de81c989bd3f85122e83a78de19beefb
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-05 13:03:32 -0800

    Time: 606 ms (61.38%) | Memory: 0B (100.00%) - LeetSync

--- 1453-ads-performance/ads-performance.sql
+++ 1453-ads-performance/ads-performance.sql
@@ -1,41 +1,16 @@
-WITH ad_clicks AS (
-    SELECT
-        ad_id,
-        COUNT(*) AS count_clicks
-    FROM
-        ads
-    WHERE
-        action = 'Clicked'
-    GROUP BY
-        ad_id
-),
-ad_views AS (
-    SELECT
-        ad_id,
-        COUNT(*) AS count_views
-    FROM
-        ads
-    WHERE
-        action = 'Viewed'
-    GROUP BY
-        ad_id
-)
 SELECT
-    a.ad_id,
-    CASE
-        WHEN IFNULL(ad_clicks.count_clicks, 0) + IFNULL(ad_views.count_views, 0) = 0 THEN 0.00
-        ELSE ROUND(IFNULL(ad_clicks.count_clicks, 0) / (IFNULL(ad_clicks.count_clicks, 0) + IFNULL(ad_views.count_views, 0)), 4) * 100
-    END AS ctr
+    ad_id,
+    ROUND(
+        IFNULL(
+            (SUM(CASE WHEN action = 'Clicked' THEN 1 ELSE 0 END) / 
+            (SUM(CASE WHEN action = 'Clicked' THEN 1 ELSE 0 END) + 
+             SUM(CASE WHEN action = 'Viewed' THEN 1 ELSE 0 END))), 
+        0) * 100, 2
+    ) AS ctr
 FROM
-    (SELECT DISTINCT ad_id FROM ads) a -- Corrected base table
-LEFT JOIN
-    ad_clicks
-ON
-    a.ad_id = ad_clicks.ad_id
-LEFT JOIN
-    ad_views
-ON
-    a.ad_id = ad_views.ad_id
+    ads
+GROUP BY
+    ad_id
 ORDER BY
     ctr DESC,
-    a.ad_id ASC;
+    ad_id ASC;
