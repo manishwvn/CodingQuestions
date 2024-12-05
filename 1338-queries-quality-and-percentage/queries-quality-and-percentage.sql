4d60303622b85979dd73d92f52e8ea758eb87ef2
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-05 02:55:35 -0800

    Time: 416 ms (40.38%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,19 @@
+select
+    query_name,
+    round(avg(rating/position), 2) as quality,
+    round(
+        count(
+        case
+            when rating < 3 then rating
+            else null
+            end
+        )
+        / count(*) * 100
+        ,2) as poor_query_percentage
+from
+    queries
+where
+    query_name is not null
+group by
+    query_name;
+
