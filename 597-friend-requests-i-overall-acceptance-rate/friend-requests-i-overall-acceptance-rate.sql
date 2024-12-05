6945ffefcbcf6142bf724ce50ee0aba71267e0b0
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-04 23:38:06 -0800

    Time: 528 ms (73.32%) | Memory: 0B (100.00%) - LeetSync

--- 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
+++ 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
@@ -1,8 +1,20 @@
-SELECT 
-    CASE 
-        WHEN (SELECT COUNT(*) FROM (SELECT DISTINCT sender_id, send_to_id FROM FriendRequest) AS req) = 0 THEN 0.00
-        ELSE ROUND(
-            (SELECT COUNT(*) FROM (SELECT DISTINCT requester_id, accepter_id FROM RequestAccepted) AS acc)::DECIMAL /
-            (SELECT COUNT(*) FROM (SELECT DISTINCT sender_id, send_to_id FROM FriendRequest) AS req)::DECIMAL, 2
-        )
-    END AS accept_rate;
+with total_friend_requests as(
+    select count(*) as total_count from(
+        select distinct sender_id, send_to_id from FriendRequest
+    ) T1
+),
+
+total_accepted_requests as (
+    select count(*) as accept_count from (
+        select distinct requester_id, accepter_id from RequestAccepted
+    ) T2
+)
+
+select 
+    case
+        when total_count = 0 then 0.00
+        else round((accept_count * 1.00 / total_count), 2) 
+        end as accept_rate
+from 
+    total_accepted_requests, 
+    total_friend_requests;
