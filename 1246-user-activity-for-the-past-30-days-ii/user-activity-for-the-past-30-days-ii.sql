daf79598f2917327a045d19359d4a4687a3c72dd
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-05 12:08:40 -0800

    Time: 392 ms (98.86%) | Memory: 0B (100.00%) - LeetSync

--- 1246-user-activity-for-the-past-30-days-ii/user-activity-for-the-past-30-days-ii.sql
+++ 1246-user-activity-for-the-past-30-days-ii/user-activity-for-the-past-30-days-ii.sql
@@ -1,13 +1,27 @@
-SELECT 
-    COALESCE(
-        ROUND(
-            CASE 
-                WHEN COUNT(DISTINCT user_id) = 0 THEN 0
-                ELSE COUNT(DISTINCT session_id) * 1.0 / COUNT(DISTINCT user_id)
-            END, 
-        2), 
-    0.00) AS average_sessions_per_user
-FROM 
-    Activity 
-WHERE activity_date:: DATE BETWEEN '2019-07-28'::DATE - INTERVAL '30 day' 
-AND '2019-07-27'::DATE
+-- select
+--     sum(select count(distinct session_id from activity)) / count(user_id)
+-- from
+--     activity
+-- where
+--     activity_date between date('2019-07-27') - INTERVAL 29 DAY and date('2019-07-27')
+-- group by
+--     user_id
+-- having
+--     count(activity_type) >= 1;
+
+with user_sessions as(
+select
+    user_id,
+    count(distinct session_id) unique_sessions
+from
+    activity
+where
+    activity_date between date('2019-07-27') - INTERVAL 29 DAY and date('2019-07-27')
+group by
+    user_id
+having count(activity_type) >= 1)
+
+select
+    round(ifnull(sum(unique_sessions) / count(user_id), 0), 2) as average_sessions_per_user
+from
+    user_sessions;
