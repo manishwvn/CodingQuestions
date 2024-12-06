3924f32617476c01db0da8ed55531f4577d63a88
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-05 21:18:56 -0800

    Time: 790 ms (41.97%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2829-find-latest-salaries/find-latest-salaries.sql
@@ -0,0 +1,15 @@
+select
+    emp_id,
+    firstname,
+    lastname,
+    max(salary) as salary,
+    department_id
+from
+    Salary
+group by
+    emp_id,
+    firstname,
+    lastname,
+    department_id
+order by
+    emp_id;
