d737d35e72b831a021862cce925a012eea60e6a6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-05 21:49:36 -0800

    Time: 364 ms (50.71%) | Memory: 0B (100.00%) - LeetSync

--- 3155-highest-salaries-difference/highest-salaries-difference.sql
+++ 3155-highest-salaries-difference/highest-salaries-difference.sql
@@ -1,19 +1,19 @@
--- select distinct
---    abs((select max(salary) from salaries where department = 'Engineering') - 
---     (select max(salary) from salaries where department = 'Marketing'))
---     as salary_difference
--- from
---     salaries;
-
-select
-    abs(
-    max(case
-        when
-            department = 'Engineering' then salary else 0 end) - 
-    max(case
-        when
-            department = 'Marketing' then salary else 0 end)) as            
-    salary_difference
+select distinct
+   abs((select max(salary) from salaries where department = 'Engineering') - 
+    (select max(salary) from salaries where department = 'Marketing'))
+    as salary_difference
 from
     salaries;
+
+-- select
+--     abs(
+--     max(case
+--         when
+--             department = 'Engineering' then salary else 0 end) - 
+--     max(case
+--         when
+--             department = 'Marketing' then salary else 0 end)) as            
+--     salary_difference
+-- from
+--     salaries;
     
