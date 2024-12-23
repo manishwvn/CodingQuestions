e2b7c72547547d861f82946b64e45be56de3095a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-23 15:15:30 -0800

    Time: 546 ms (81.10%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,32 +1,27 @@
--- select
---     *
--- from
---     users u
--- where
---     u.banned = 'No';
+with cancelled_trips as (
+    select
+        t.request_at as day,
+        case when t.status <> 'completed' then 1 else 0 end as cancelled
+    from
+        trips t
+    join
+        users u1
+    on
+        t.client_id = u1.users_id
+        and
+        u1.banned = 'No'
+    join
+        users u2
+    on
+        t.driver_id = u2.users_id
+        and
+        u2.banned = 'No'
+    where
+        date(request_at) between date('2013-10-01') and date('2013-10-03'))
 
 select
-    request_at as Day,
-    round(sum(case when status <> 'completed' then 1 else 0 end) / count(*), 2) as 'Cancellation Rate'
+    day,
+    round(sum(cancelled) / count(cancelled), 2) as 'Cancellation Rate'
 from
-    trips t
-where
-    client_id in (
-        select
-            users_id
-        from
-            users
-        where
-            role = 'client' and banned = 'No')
-    and
-    driver_id in (
-        select
-            driver_id
-        from
-            users
-        where
-            role = 'driver' and banned = 'No')
-    and
-        date(request_at) between date('2013-10-01') and date('2013-10-03')
-group by
-    Day;
+    cancelled_trips
+group by day;
