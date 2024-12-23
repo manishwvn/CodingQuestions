38d57b94aa1d6ca5d264a5fcfe9c1b6ac0e2adeb
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-22 23:34:05 -0800

    Time: 501 ms (32.96%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,9 +1,20 @@
-SELECT 
-    A.NAME AS 'EMPLOYEE'
-FROM
-    EMPLOYEE AS A,
-    EMPLOYEE AS B
-WHERE
-    A.MANAGERID = B.ID
-    AND
-    A.SALARY > B.SALARY;
+-- SELECT 
+--     A.NAME AS 'EMPLOYEE'
+-- FROM
+--     EMPLOYEE AS A,
+--     EMPLOYEE AS B
+-- WHERE
+--     A.MANAGERID = B.ID
+--     AND
+--     A.SALARY > B.SALARY;
+
+select
+    a.name as Employee
+from
+    employee a
+join
+    employee b
+on
+    a.managerid = b.id
+where
+    a.salary > b.salary;
