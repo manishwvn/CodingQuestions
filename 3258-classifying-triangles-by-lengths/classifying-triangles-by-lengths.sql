0a61bf1b6d3f69bdf0b8c26aae248205effc6c43
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-27 00:42:50 -0800

    Time: 1216 ms (5.21%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3258-classifying-triangles-by-lengths/classifying-triangles-by-lengths.sql
@@ -0,0 +1,16 @@
+select
+    case
+        when a + b > c and b + c > a and a + c > b then (
+            case
+                when
+                    a = b and b = c then 'Equilateral'
+                when
+                    a = b or b = c or c = a then 'Isosceles'
+                else
+                    'Scalene'
+            end
+
+        )
+        else 'Not A Triangle' end as triangle_type
+from
+    triangles;
