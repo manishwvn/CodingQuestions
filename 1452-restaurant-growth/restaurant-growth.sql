673069a5bfd70d38b79c34d140c715e9a627c6f2
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-27 15:18:52 -0800

    Time: 760 ms (9.26%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,42 +1,22 @@
--- with daily_sales as(
---     select
---         visited_on,
---         sum(amount) as daily_amount
---     from
---         customer
---     group by
---         visited_on
--- ),
--- moving_sales as(
---     select
---         visited_on,
---         sum(daily_amount) over(order by visited_on rows between 6 preceding and current row) as week_amount,
---         count(distinct daily_amount) over(order by visited_on rows between 6 preceding and current row) as days
---     from
---         daily_sales 
--- )
+with cte as (select
+    visited_on,
+    sum(amount) as total_amount
+from
+    customer
+group by
+    visited_on),
 
--- select
---     visited_on,
---     week_amount as amount,
---     round(week_amount / days, 2) as average_amount
--- from
---     moving_sales
--- -- where
--- --     visited_on >= date('2019-01-07');
+cte2 as (
+    select
+        visited_on,
+        sum(total_amount) over(order by visited_on rows between 6 preceding and current row) as amount,
+        round(avg(total_amount) over(order by visited_on rows between 6 preceding and current row), 2) as average_amount
+    from
+        cte)
 
-WITH TEMP1 AS (
-    SELECT visited_on, SUM(amount) amount 
-    FROM Customer 
-    GROUP BY visited_on 
-    ORDER BY visited_on
-),
-TEMP2 AS(
-SELECT visited_on,
-SUM(amount) over(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) as amount,
-ROUND(AVG(amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) AS average_amount,
-ROW_NUMBER() OVER(ORDER BY visited_on) AS R1
-FROM TEMP1)
-SELECT visited_on, amount, average_amount
-FROM TEMP2
-WHERE R1>6;
+select
+    *
+from
+    cte2
+where
+    visited_on >= (select visited_on from cte2 order by visited_on limit 1) + 6;
