a062567aeaaa8bf0d51fb5335239a11e853918a1
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-28 14:27:32 -0800

    Time: 687 ms (6.80%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,8 +1,28 @@
-SELECT
-    name, population, area
-FROM
-    WORLD
-WHERE
-    AREA >= 3000000
-    OR 
-    POPULATION >= 25000000;
+# SELECT 
+#     NAME AS 'name',
+#     POPULATION AS 'population',
+#     AREA AS 'area'
+# FROM 
+#     WORLD
+# WHERE 
+#     AREA >= 3000000 OR 
+#     POPULATION >= 25000000;
+
+
+WITH BIG_COUNTRIES AS (
+    SELECT 
+        NAME AS name,
+        POPULATION AS population,
+        AREA as area
+    FROM 
+        WORLD
+    WHERE 
+        AREA >= 3000000 
+        OR 
+        POPULATION >= 25000000
+)
+
+SELECT 
+    * 
+FROM 
+    BIG_COUNTRIES;
