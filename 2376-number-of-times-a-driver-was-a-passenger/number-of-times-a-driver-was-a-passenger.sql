52a6a1d3a1d9ca5b6beff8a720cd506bcef263b0
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-27 18:31:44 -0800

    Time: 710 ms (28.78%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2376-number-of-times-a-driver-was-a-passenger/number-of-times-a-driver-was-a-passenger.sql
@@ -0,0 +1,11 @@
+select
+    distinct r1.driver_id,
+    count(distinct r2.ride_id) as cnt
+from
+    rides r1
+left join
+    rides r2
+on
+    r1.driver_id = r2.passenger_id
+group by
+    r1.driver_id
