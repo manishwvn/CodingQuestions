6542f061479e9e7f4b5242d3d72a0eaa806c04c8
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-28 19:32:56 -0800

    Time: 2448 ms (14.25%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,20 +1,5 @@
-with first_years as (
-    select
-        product_id,
-        min(year) as first_year
-    from
-        Sales
-        group by product_id
-)
-
-select
-    s.product_id,
-    s.year as first_year,
-    s.quantity,
-    s.price
-from
-    Sales s
-join
-    first_years f
-on
-    s.product_id = f.product_id and s.year = f.first_year;
+SELECT product_id, year AS first_year, quantity, price
+FROM (SELECT *,
+            RANK() OVER (PARTITION BY product_id ORDER BY year) AS rnk
+FROM Sales) a
+WHERE rnk = 1;
