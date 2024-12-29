669239fdf33eaa7f40a58aca562f8100c110eb1a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-29 01:29:25 -0800

    Time: 1422 ms (25.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
@@ -0,0 +1,12 @@
+select
+    count(a.account_id) as accounts_count
+from
+    subscriptions a
+left join
+    streams s
+on
+    a.account_id = s.account_id
+where
+    year(end_date) = 2021
+    and
+    year(stream_date) <> 2021
