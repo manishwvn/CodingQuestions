756b091b30a5ec8936db8039577e5257aa948c42
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2024-12-29 00:29:29 -0800

    Time: 2553 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
@@ -0,0 +1,22 @@
+select
+    t.team_id,
+    t.team_name,
+    sum(
+        (case
+            when t.team_id = m.host_team and m.host_goals > m.guest_goals then 3
+            when t.team_id = m.guest_team and m.guest_goals > m.host_goals then 3
+            when m.host_goals = m.guest_goals then 1
+            else 0
+        end)) as num_points
+from
+    teams t
+left join
+    matches m
+on
+    t.team_id = m.host_team
+or
+    t.team_id = m.guest_team
+group by
+    t.team_id
+order by
+    3 desc, 1;
