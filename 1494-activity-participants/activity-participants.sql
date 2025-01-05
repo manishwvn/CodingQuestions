ea9cce2476d5c5f0768388119220787ddb9617fb
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-05 02:36:43 -0800

    Time: 1096 ms (30.11%) | Memory: 0B (100.00%) - LeetSync

--- 1494-activity-participants/activity-participants.sql
+++ 1494-activity-participants/activity-participants.sql
@@ -1,7 +1,23 @@
-with cte as (select
+-- with cte as (select
+--     activity,
+--     dense_rank() over(order by count(id)) as min_rnk,
+--     dense_rank() over(order by count(id) desc) as max_rnk
+-- from
+--     friends
+-- group by
+--     activity)
+
+-- select
+--     activity
+-- from
+--     cte
+-- where
+--     min_rnk <> 1 and max_rnk <> 1;
+
+with cte as (
+select
     activity,
-    dense_rank() over(order by count(id)) as min_rnk,
-    dense_rank() over(order by count(id) desc) as max_rnk
+    count(id) as counts
 from
     friends
 group by
@@ -12,4 +28,6 @@ select
 from
     cte
 where
-    min_rnk <> 1 and max_rnk <> 1;
+    counts not in (select max(counts) from cte)
+and
+    counts not in (select min(counts) from cte);
