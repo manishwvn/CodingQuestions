d3904a78482dd8fa9eb92c08e143c19dd28aac9a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-05 01:54:29 -0800

    Time: 1154 ms (14.77%) | Memory: 0B (100.00%) - LeetSync

--- 1399-page-recommendations/page-recommendations.sql
+++ 1399-page-recommendations/page-recommendations.sql
@@ -1,10 +1,3 @@
--- SELECT DISTINCT l.page_id AS recommended_page
--- FROM Friendship f
--- INNER JOIN Likes l
---   ON (l.user_id = f.user2_id AND f.user1_id = 1) OR (l.user_id = f.user1_id AND f.user2_id = 1)
--- WHERE l.page_id NOT IN (SELECT DISTINCT page_id FROM Likes WHERE user_id = 1)
-
-
 select
     distinct page_id as recommended_page
 from
