f27d9893ce459b4515cda50c7a0eeef08f896643
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-06 01:03:32 -0800

    Time: 974 ms (49.76%) | Memory: 0B (100.00%) - LeetSync

--- 3624-find-top-performing-driver/find-top-performing-driver.sql
+++ 3624-find-top-performing-driver/find-top-performing-driver.sql
@@ -1,4 +1,9 @@
-WITH cte AS (
+SELECT
+    fuel_type,
+    driver_id,
+    rating,
+    distance
+FROM (
     SELECT
         v.fuel_type,
         d.driver_id,
@@ -10,21 +15,13 @@ WITH cte AS (
         ) AS rnk
     FROM
         trips t
-    LEFT JOIN
+    JOIN
         vehicles v ON t.vehicle_id = v.vehicle_id
     JOIN
         drivers d ON v.driver_id = d.driver_id
     GROUP BY
         v.fuel_type, d.driver_id
-)
-
-SELECT
-    fuel_type,
-    driver_id,
-    rating,
-    distance
-FROM
-    cte
+) ranked
 WHERE
     rnk = 1
 ORDER BY
