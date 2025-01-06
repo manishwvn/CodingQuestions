028159dd2bd50c1171d38e32c61c9bac91565cd0
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-06 00:57:58 -0800

    Time: 2054 ms (7.04%) | Memory: 0B (100.00%) - LeetSync

--- 3624-find-top-performing-driver/find-top-performing-driver.sql
+++ 3624-find-top-performing-driver/find-top-performing-driver.sql
@@ -14,36 +14,18 @@ WITH cte AS (
         vehicles v ON t.vehicle_id = v.vehicle_id
     JOIN
         drivers d ON v.driver_id = d.driver_id
-    WHERE
-        v.fuel_type = 'Electric'
-    GROUP BY
-        v.fuel_type, d.driver_id
-),
-
-cte2 AS (
-    SELECT
-        v.fuel_type,
-        d.driver_id,
-        ROUND(AVG(t.rating), 2) AS rating,
-        SUM(t.distance) AS distance,
-        DENSE_RANK() OVER (
-            PARTITION BY v.fuel_type
-            ORDER BY ROUND(AVG(t.rating), 2) DESC, SUM(t.distance) DESC, d.accidents ASC
-        ) AS rnk
-    FROM
-        trips t
-    LEFT JOIN
-        vehicles v ON t.vehicle_id = v.vehicle_id
-    JOIN
-        drivers d ON v.driver_id = d.driver_id
-    WHERE
-        v.fuel_type <> 'Electric'
     GROUP BY
         v.fuel_type, d.driver_id
 )
 
-SELECT fuel_type, driver_id, rating, distance FROM cte WHERE rnk = 1
-UNION
-SELECT fuel_type, driver_id, rating, distance FROM cte2 WHERE rnk = 1
-order by
+SELECT
+    fuel_type,
+    driver_id,
+    rating,
+    distance
+FROM
+    cte
+WHERE
+    rnk = 1
+ORDER BY
     fuel_type;
