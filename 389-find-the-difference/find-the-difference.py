b2490717fb599ebb5b3dbb7e5d3e3fa71ffc2d76
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-13 04:44:17 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (42.22%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,10 +1,5 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        ch = 0
-        for char in s:
-            ch ^= ord(char)
 
-        for char in t:
-            ch ^= ord(char)
-
-        return chr(ch)
+        return list((Counter(t) - Counter(s)).keys())[0]
+        
