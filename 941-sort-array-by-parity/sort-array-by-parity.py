80c11ff3f3e02a7fc3923eda3d97c583b5404e86
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-13 04:59:57 -0800

    Time: 0 ms (100.00%) | Memory: 18.4 MB (24.79%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -3,14 +3,11 @@ class Solution:
 
         l, r = 0, 0
 
-        while l < len(nums) and r < len(nums):
+        while r < len(nums):
             if nums[r] % 2 == 0:
                 nums[l], nums[r] = nums[r], nums[l]
                 l += 1
-                r += 1
-            else:
-                r += 1
-        
+            r += 1
         return nums
 
         
