cd49ccf22ecddd57a9440c9d13c1aee7317f8a86
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-14 00:31:12 -0800

    Time: 48 ms (5.03%) | Memory: 18.8 MB (5.98%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
@@ -1,12 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, word):
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+            curr.count += 1
+
+    def count(self, pref):
+        curr = self.root
+        for char in pref:
+            if char not in curr.children:
+                return 0
+            curr = curr.children[char]
+        return curr.count
+
 class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
 
-        n = len(pref)
-        count = 0
+
+        trie = Trie()
 
         for word in words:
-            if pref == word[:n]:
-                count += 1
-        
-        return count
+            if len(word) >= len(pref):
+                trie.add(word)
+        return trie.count(pref)
         
