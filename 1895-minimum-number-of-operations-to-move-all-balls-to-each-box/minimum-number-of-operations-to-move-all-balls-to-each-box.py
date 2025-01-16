e8ac20a441bb9e2579a79301fe4206c04d472ffb
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-15 17:40:02 -0800

    Time: 11 ms (60.57%) | Memory: 17.8 MB (93.20%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,23 +1,22 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         n = len(boxes)
-        answer = [0] * n
-        # Left-to-right pass
-        count = 0  # Number of balls encountered so far
-        operations = 0  # Cumulative operations to move balls from the left
-        
+        result = [0] * n
+
+        count = 0
+        ops = 0
         for i in range(n):
-            answer[i] += operations
-            count += int(boxes[i])  # Add the current box's ball if it's '1'
-            operations += count  # Increment operations by the number of balls
+            result[i] += ops
+            count += int(boxes[i])
+            ops += count
+
+        count = ops = 0
+
+        for i in range(n-1, -1, -1):
+            result[i] += ops
+            count += int(boxes[i])
+            ops += count
+
+        return result
         
-        # Right-to-left pass
-        count = 0  # Reset the ball count
-        operations = 0  # Reset the cumulative operations
         
-        for i in range(n - 1, -1, -1):
-            answer[i] += operations
-            count += int(boxes[i])  # Add the current box's ball if it's '1'
-            operations += count  # Increment operations by the number of balls
-        
-        return answer
