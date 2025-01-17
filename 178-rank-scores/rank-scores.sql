70183be6b46e4e0960a1aabaa89a37c3f07860bd
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-17 15:36:25 -0800

    Time: 1128 ms (7.12%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,7 +1,14 @@
-SELECT
-    S.SCORE, 
-    DENSE_RANK() OVER (ORDER BY S.SCORE DESC) AS 'rank'
-FROM
-    SCORES S
-ORDER BY
-    1 DESC;
+select
+    s1.score,
+    count(distinct s2.score) as 'rank'
+from
+    scores s1
+join
+    scores s2
+on
+    s1.score <= s2.score
+group by
+    s1.id,
+    s1.score
+order by
+    s1.score desc;
