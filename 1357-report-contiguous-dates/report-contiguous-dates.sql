5baacdff1deb2a19a476dec23eee26d2397d7ea4
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-21 02:17:31 -0800

    Time: 2351 ms (23.84%) | Memory: 0B (100.00%) - LeetSync

--- 1357-report-contiguous-dates/report-contiguous-dates.sql
+++ 1357-report-contiguous-dates/report-contiguous-dates.sql
@@ -13,14 +13,15 @@ from
 cte2 as (
 select
     *,
-    row_number() over(partition by state order by date) as rnk
+    row_number() over(order by date) as date_rnk,
+    row_number() over(partition by state order by date) as state_rnk
 from
     cte),
 
 cte3 as (
     select
         *,
-        date_sub(date, interval rnk day) as grp
+        date_rnk - state_rnk as grp
     from
         cte2
 )
