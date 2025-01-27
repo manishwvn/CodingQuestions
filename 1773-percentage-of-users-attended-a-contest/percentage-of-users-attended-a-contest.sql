152b94532b07585bb9a8a5996fb97ccda9441de4
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-26 17:53:56 -0800

    Time: 2349 ms (13.89%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,14 +1,12 @@
 select
     r.contest_id,
-    round(count(distinct r.user_id) / (select count(1) from users) * 100, 2) as percentage
+    round(count(distinct r.user_id) / count(distinct u.user_id) * 100, 2)
+    as percentage
 from
-    register r
-left join
     users u
-on
-    r.user_id = u.user_id
+cross join
+    register r
 group by
     r.contest_id
 order by
-    percentage desc, r.contest_id asc;
-    
+    percentage desc, r.contest_id
