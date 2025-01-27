4bfc103ac46949f5d51318e66d45dc1a61eccc8f
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-27 01:56:54 -0800

    Time: 465 ms (73.69%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,8 +1,21 @@
-select case when count(*) > 0 then max(num)
-            else null
-            end as num from (select
-    num
+-- select case when count(*) > 0 then max(num)
+--             else null
+--             end as num from (select
+--     num
+-- from
+--     MyNumbers
+-- group by num
+-- having count(num) = 1) as t;
+with singlenums as (
+    select
+        num, count(*) as count
+    from
+        mynumbers
+    group by
+        num
+    having count(*) = 1)
+
+select
+    max(num) as num
 from
-    MyNumbers
-group by num
-having count(num) = 1) as t;
+    singlenums;
