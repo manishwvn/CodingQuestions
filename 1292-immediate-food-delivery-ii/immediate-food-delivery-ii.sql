e00778fe1651b51434541adafbdb01cd63ba2640
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-26 18:06:54 -0800

    Time: 1144 ms (42.37%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -3,7 +3,7 @@ WITH RankedOrders AS (
         customer_id,
         order_date,
         customer_pref_delivery_date,
-        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS order_rank
+        DENSE_RANK() OVER (PARTITION BY customer_id ORDER BY order_date) AS order_rank
     FROM
         delivery
 )
