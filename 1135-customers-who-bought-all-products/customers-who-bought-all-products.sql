4af7ac44e1e9e9b41f7743d5f6cd812efbd185d7
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-27 02:02:31 -0800

    Time: 1155 ms (26.43%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,17 +1,8 @@
-SELECT
-  c.customer_id
-FROM
-  Customer c
-join
-    product p
-on
-    c.product_key = p.product_key
-GROUP BY
-  customer_id
-HAVING
-  COUNT(DISTINCT c.product_key) = (
-    SELECT
-      COUNT(product_key)
-    FROM
-      Product
-  );
+select
+    customer_id
+from
+    customer
+group by
+    customer_id
+having
+    count(distinct product_key) = (select count(distinct product_key) from product);
