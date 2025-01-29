524cc198e678683646489863a7765dd19a2cbdec
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-29 15:32:19 -0800

    Time: 820 ms (59.44%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,28 +1,17 @@
-with same_locs as(
-    select
-        concat(lat,',',lon) as loc
-    from
-        insurance
-    group by lat, lon
-    having count(pid) > 1
-),
-
-inv_2015 as(
-    select
-        distinct
-            t1.pid,
-            t1.tiv_2015,
-            t1.tiv_2016
-    from
-        insurance t1
-    left join
-        insurance t2
-    on
-        t1.tiv_2015 = t2.tiv_2015
-    where
-        t1.pid <> t2.pid
-    and
-        concat(t1.lat,',',t1.lon) not in (select loc from same_locs)
-)
-
-select round(sum(tiv_2016), 2) as tiv_2016 from inv_2015;
+select
+    round(sum(tiv_2016), 2) as tiv_2016
+from
+    insurance
+where
+    tiv_2015 in (
+        select
+            tiv_2015
+        from
+            insurance
+        group by
+            tiv_2015
+        having
+            count(pid) > 1
+    )
+and
+    (lat, lon) in (select lat, lon from insurance group by lat, lon having count(pid) = 1);
