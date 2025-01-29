95e8bdece415fcace4e5418b6398dd2c6c2fd7e9
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-29 00:52:45 -0800

    Time: 591 ms (48.75%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,20 +1,27 @@
--- with cte as (select
---     visited_on,
---     sum(amount) over(order by visited_on range between interval 6 day preceding and current row) as rolling_sum,
---     avg(amount) over(order by visited_on range between interval 6 day preceding and current row) as rolling_avg
+-- with cte as (
+--     select
+--         visited_on,
+--         sum(amount) as daily_amount
+--     from
+--         customer
+--     group by
+--         visited_on),
+
+-- cte2 as (select
+--     *,
+--     sum(daily_amount) over(order by visited_on range between interval 6 day preceding and current row) as rolling_sum,
+--     avg(daily_amount) over(order by visited_on range between interval 6 day preceding and current row) as rolling_avg
 -- from
---     customer
--- group by
---     visited_on)
+--     cte)
 
 -- select
 --     visited_on,
 --     rolling_sum as amount,
---     round(rolling_avg,2) as average_amount
+--     round(rolling_avg, 2) as average_amount
 -- from
---     cte
+--     cte2
 -- where
---     visited_on >= (select min(visited_on) + interval 6 day as visited_on from customer)
+--     visited_on >= (select min(visited_on) + interval 6 day from customer)
 -- order by
 --     visited_on;
 
@@ -26,21 +33,30 @@ with cte as (
         customer
     group by
         visited_on),
-
-cte2 as (select
-    *,
-    sum(daily_amount) over(order by visited_on range between interval 6 day preceding and current row) as rolling_sum,
-    avg(daily_amount) over(order by visited_on range between interval 6 day preceding and current row) as rolling_avg
-from
-    cte)
+cte2 as (
+select 
+    c1.visited_on as visited_on1,
+    c1.daily_amount as amount1,
+    c2.visited_on as visited_on2,
+    c2.daily_amount as amount2
+from 
+    cte c1 
+join 
+    cte c2 
+on 
+    c1.visited_on >= c2.visited_on
+where
+    datediff(c1.visited_on, c2.visited_on) between 0 and 6
+    and
+    c1.visited_on >= (select min(visited_on) + interval 6 day from customer))
 
 select
-    visited_on,
-    rolling_sum as amount,
-    round(rolling_avg, 2) as average_amount
+    visited_on1 as visited_on,
+    sum(amount2) as amount,
+    round(avg(amount2),2) as average_amount
 from
     cte2
-where
-    visited_on >= (select min(visited_on) + interval 6 day from customer)
+group by
+    1
 order by
-    visited_on;
+    1;
