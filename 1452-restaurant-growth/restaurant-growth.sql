a60077f8c55489ef43faf81636cc9795bbd02042
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-29 00:31:44 -0800

    Time: 762 ms (22.18%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,24 +1,46 @@
-with cte as (select
-    visited_on,
-    sum(amount) as total_amount
-from
-    customer
-group by
-    visited_on),
+-- with cte as (select
+--     visited_on,
+--     sum(amount) over(order by visited_on range between interval 6 day preceding and current row) as rolling_sum,
+--     avg(amount) over(order by visited_on range between interval 6 day preceding and current row) as rolling_avg
+-- from
+--     customer
+-- group by
+--     visited_on)
 
-cte2 as (
+-- select
+--     visited_on,
+--     rolling_sum as amount,
+--     round(rolling_avg,2) as average_amount
+-- from
+--     cte
+-- where
+--     visited_on >= (select min(visited_on) + interval 6 day as visited_on from customer)
+-- order by
+--     visited_on;
+
+with cte as (
     select
         visited_on,
-        sum(total_amount) over(order by visited_on rows between 6 preceding and current row) as amount,
-        round(avg(total_amount) over(order by visited_on rows between 6 preceding and current row), 2) as average_amount
+        sum(amount) as daily_amount
     from
-        cte)
+        customer
+    group by
+        visited_on),
+
+cte2 as (select
+    *,
+    sum(daily_amount) over(order by visited_on range between interval 6 day preceding and current row) as rolling_sum,
+    avg(daily_amount) over(order by visited_on range between interval 6 day preceding and current row) as rolling_avg
+from
+    cte)
 
 select
-    *
+    visited_on,
+    rolling_sum as amount,
+    round(rolling_avg, 2) as average_amount
 from
     cte2
 where
-    visited_on >= (select visited_on from cte2 order by visited_on limit 1) + 6
+    visited_on >= (select min(visited_on) + interval 6 day from customer)
 order by
     visited_on;
