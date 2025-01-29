d89f17965c1c9d2f3385b50a25d536f56e290f0f
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-28 21:55:08 -0800

    Time: 764 ms (19.71%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,20 +1,22 @@
-with cte as (
-    select
-        *,
-        lead(id) over(order by id) as next,
-        lag(id) over(order by id) as prev
-    from
-        seat
-)
+-- if id is even then decrease by 1
+-- if id is odd 
+    -- and id = max then keep as it is
+    -- else:
+    --     id + 1
+with cte as (select
+    *,
+    case
+        when id % 2 = 0 then id - 1
+        when id % 2 = 1 and id = (select max(id) from seat) then id
+        else id + 1
+    end as new_id
+from
+    seat)
 
 select
-    case
-        when ((id % 2) = 1 and next is not null) then next
-        when (id % 2) = 0 then prev
-        else id
-    end as id,
+    new_id as id,
     student
 from
     cte
 order by
-    id;
+    new_id;
