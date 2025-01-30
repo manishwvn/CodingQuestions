9984f3a66b75588a8aed8fb54192ecf37f356d42
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-29 22:37:14 -0800

    Time: 1121 ms (15.93%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,12 +1,14 @@
-select customer_number
-from (
-    select
-        customer_number,
-        count(*) as orders,
-        row_number() over (order by count(*) desc) as rnk
-    from
-        orders
-    group by
-        customer_number
-) ranked_orders
-where rnk = 1;
+with cte as (select
+    customer_number,
+    dense_rank() over(order by count(*) desc) as rnk
+from
+    orders
+group by
+    customer_number)
+
+select
+    customer_number
+from
+    cte
+where
+    rnk = 1;
