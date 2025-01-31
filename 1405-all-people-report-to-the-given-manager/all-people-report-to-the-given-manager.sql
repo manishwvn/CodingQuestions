8cdbccdc38d1804885b667cada6153d3dbf201b0
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-31 12:58:00 -0800

    Time: 591 ms (15.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
@@ -0,0 +1,16 @@
+with recursive cte as (
+
+    
+    select employee_id from employees where manager_id = 1 and employee_id <> 1
+    union
+    
+    select e.employee_id
+    from
+        employees e
+    join
+        cte c
+    on
+        e.manager_id = c.employee_id)
+
+
+select * from cte;
