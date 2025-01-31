3e662be2e9a3db53c116ab161ff9eacb5b815fd0
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-31 12:10:49 -0800

    Time: 1362 ms (13.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1852-biggest-window-between-visits/biggest-window-between-visits.sql
@@ -0,0 +1,25 @@
+with cte as (select
+    *,
+    lead(visit_date) over(partition by user_id order by visit_date) as next_date
+from
+    uservisits),
+
+cte2 as(
+select
+    user_id,
+    case
+        when next_date is null then datediff(date('2021-01-01'), visit_date)
+        else datediff(next_date, visit_date)
+    end as windows
+from
+    cte)
+
+select
+    user_id,
+    max(windows) as biggest_window
+from
+    cte2
+group by
+    user_id
+order by
+    user_id;
