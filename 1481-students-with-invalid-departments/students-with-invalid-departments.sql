8b09ad06337e278dfcacab9cd6db1d71f9e724c6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-01-31 10:31:28 -0800

    Time: 1219 ms (62.46%) | Memory: 0B (100.00%) - LeetSync

--- 1481-students-with-invalid-departments/students-with-invalid-departments.sql
+++ 1481-students-with-invalid-departments/students-with-invalid-departments.sql
@@ -1,15 +1,2 @@
-with cte as (select
-    distinct d.id
-from
-    departments d
-join
-    students s
-on
-    d.id = s.department_id)
-
-select
-    id, name
-from
-    students
-where
-    department_id not in (select id from cte)
+SELECT id, name FROM Students
+WHERE department_id not in (SELECT id from Departments)
