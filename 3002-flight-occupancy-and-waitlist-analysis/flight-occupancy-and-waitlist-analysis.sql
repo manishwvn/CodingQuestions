3f801eaf3ff772ed908cacd8c3888567c7931904
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-02-01 14:48:36 -0800

    Time: 1370 ms (67.45%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3002-flight-occupancy-and-waitlist-analysis/flight-occupancy-and-waitlist-analysis.sql
@@ -0,0 +1,29 @@
+with cte as (select
+    f.*,
+    p.passenger_id,
+    count(passenger_id) over(partition by flight_id) as p_count
+from
+    flights f
+left join
+    passengers p
+on
+    f.flight_id = p.flight_id)
+
+select
+    flight_id,
+    case
+        when
+            p_count <= capacity then p_count
+        else
+            capacity
+    end as booked_cnt,
+    case
+        when
+            p_count > capacity then p_count - capacity
+        else
+            0
+    end as waitlist_cnt
+from
+    cte
+group by
+    flight_id
