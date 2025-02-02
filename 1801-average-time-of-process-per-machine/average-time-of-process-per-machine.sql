61d7ac67645f11c6fcb814e5fd8139793b11ca51
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-02-01 18:34:32 -0800

    Time: 533 ms (15.86%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,6 +1,6 @@
 select
     m1.machine_id,
-    abs(round(avg(m1.timestamp - m2.timestamp), 3)) as processing_time
+    round(avg(m2.timestamp - m1.timestamp), 3) as processing_time
 from
     activity m1
 join
@@ -11,6 +11,8 @@ on
     m1.process_id = m2.process_id
     and
     m1.activity_type = 'start' and m2.activity_type = 'end'
+    and
+    m1.timestamp < m2.timestamp
 group by
     1;
 
