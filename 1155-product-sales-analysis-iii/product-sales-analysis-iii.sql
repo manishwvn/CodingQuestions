3847929f5bbe413309c455fe8443cf164ceb403d
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-02-01 18:33:33 -0800

    Time: 2503 ms (31.01%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,5 +1,5 @@
 SELECT product_id, year AS first_year, quantity, price
 FROM (SELECT *,
-            RANK() OVER (PARTITION BY product_id ORDER BY year) AS rnk
+            DENSE_RANK() OVER (PARTITION BY product_id ORDER BY year) AS rnk
 FROM Sales) a
 WHERE rnk = 1;
