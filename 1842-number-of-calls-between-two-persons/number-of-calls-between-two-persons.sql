0903fd5d8ca83c8b2b4d1fe9249edd4ea52ab4d6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-02-03 10:20:10 -0800

    Time: 1572 ms (8.67%) | Memory: 0B (100.00%) - LeetSync

--- 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
+++ 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
@@ -23,3 +23,12 @@ where
     person1 < person2
 group by
     person1, person2;
+
+
+-- SELECT 
+--     LEAST(from_id, to_id) AS person1, 
+--     GREATEST(from_id, to_id) AS person2, 
+--     COUNT(*) AS call_count, 
+--     SUM(duration) AS total_duration
+-- FROM Calls
+-- GROUP BY 1, 2
