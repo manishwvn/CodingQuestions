7c4de7dd1b35adfb83c89947b571d4cc0092473b
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-02-04 15:05:58 -0800

    Time: 0 ms (100.00%) | Memory: 18.7 MB (60.33%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        prev = float('inf')
+        curr_profit, max_profit = 0, 0
+
+        for curr in prices:
+            if curr < prev:
+                prev = curr
+            else:
+                curr_profit = curr - prev
+                max_profit += curr_profit
+                prev = curr
+        return max_profit
+        
