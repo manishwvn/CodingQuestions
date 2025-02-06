f14454cb6694bdf20eb76763f48cfbb6473eab68
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-02-06 07:34:12 -0800

    Time: 512 ms (36.54%) | Memory: 0B (100.00%) - LeetSync

--- 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
+++ 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
@@ -1,21 +1,39 @@
-WITH AnswerRates AS (
-    SELECT
+with shown as (
+    select
+        id,
         question_id,
-        COALESCE(
-            SUM(CASE WHEN action = 'answer' THEN 1 ELSE 0 END)::decimal /
-            NULLIF(SUM(CASE WHEN action = 'show' THEN 1 ELSE 0 END), 0),
-            0
-        ) AS answer_rate
-    FROM
-        SurveyLog
-    GROUP BY
-        question_id
+        count(*) as num_shown
+    from
+        surveylog
+    where
+        action = 'show'
+    group by
+        id, question_id
+),
+
+answered as (
+    select
+        id, question_id, answer_id, count(*) as num_ans
+    from
+        surveylog
+    where
+        action = 'answer'
+    group by
+        id, question_id
 )
-SELECT
-    question_id AS survey_log
-FROM
-    AnswerRates
-ORDER BY
-    answer_rate DESC,
-    question_id ASC
-LIMIT 1;
+
+
+select
+    s.question_id as survey_log
+from
+    shown s
+left join
+    answered a
+on
+    s.id = a.id
+    and
+    s.question_id = a.question_id
+order by
+    coalesce(a.num_ans, 0) / s.num_shown desc, s.question_id asc
+limit
+    1;
