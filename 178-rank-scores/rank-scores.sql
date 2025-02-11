8f99a239bc7d37e94e4afcab71d9a9bac2695171
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-02-10 18:03:59 -0800

    Time: 354 ms (90.41%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,14 +1,7 @@
-select
-    s1.score,
-    (
-        select
-            count(distinct s2.score)
-        from
-            scores s2
-        where
-            s2.score >= s1.score
-    ) as 'rank'
-from
-    scores s1
-order by
-    s1.score desc;
+SELECT
+    S.SCORE, 
+    DENSE_RANK() OVER (ORDER BY S.SCORE DESC) AS 'rank'
+FROM
+    SCORES S
+ORDER BY
+    1 DESC;
