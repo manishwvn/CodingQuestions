937ca26621445885caed21d3153d4b639eccf7f4
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-03-13 14:41:02 -0700

    Time: 849 ms (13.76%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3816-dna-pattern-recognition/dna-pattern-recognition.sql
@@ -0,0 +1,14 @@
+select
+    *,
+    case when dna_sequence like 'ATG%' then 1 else 0 end as has_start,
+    case
+        when dna_sequence like '%TAA' or dna_sequence like '%TAG' or dna_sequence like '%TGA'
+            then 1
+        else 0
+    end as has_stop,
+    case when dna_sequence like '%ATAT%' then 1 else 0 end as has_atat,
+    case when dna_sequence like '%ggg%' then 1 else 0 end as has_ggg
+from
+    Samples
+order by
+    sample_id;
