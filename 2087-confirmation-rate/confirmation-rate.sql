7c92eb462d0d42d14887fbe41a63a55d0ef60583
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-04-21 00:07:18 -0400

    Time: 702 ms (69.95%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,19 +1,6 @@
--- SELECT
---     S.USER_ID,
---     ROUND(AVG(CASE WHEN C.ACTION = 'confirmed' then 1 else 0 end), 2)
---         as confirmation_rate
--- FROM
---     SIGNUPS S
--- LEFT JOIN
---     CONFIRMATIONS C
--- ON
---     S.USER_ID = C.USER_ID
--- GROUP BY S.USER_ID;
-
 select
     s.user_id,
-    round(sum(case when c.action = 'confirmed' then 1 else 0 end) / count(*), 2)
-    as confirmation_rate
+    round(sum(case when c.action = 'confirmed' then 1 else 0 end) / count(*), 2) as confirmation_rate
 from
     signups s
 left join
