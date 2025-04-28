42b37d9ab88a7780c3e9682e52ff5e53f476ff17
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-04-28 02:44:39 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (61.44%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,28 +1,45 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
 
-        queue = deque()
+        # queue = deque()
+        # whites = 0
+
+        # for i in range(k):
+        #     if blocks[i] == "W":
+        #         whites += 1
+        #     queue.append(blocks[i])
+
+        # res = whites
+
+        # for i in range(k, len(blocks)):
+
+        #     if queue.popleft() == "W":
+        #         whites -= 1
+
+        #     if blocks[i] == "W":
+        #         whites += 1
+        #     queue.append(blocks[i])
+
+        #     res = min(whites, res)
+
+        # return res
+
         whites = 0
-
-        for i in range(k):
-            if blocks[i] == "W":
+        res = float("inf")
+        l = 0
+        for r in range(len(blocks)):
+            if blocks[r] == "W":
                 whites += 1
-            queue.append(blocks[i])
 
-        res = whites
-
-        for i in range(k, len(blocks)):
-
-            if queue.popleft() == "W":
-                whites -= 1
-
-            if blocks[i] == "W":
-                whites += 1
-            queue.append(blocks[i])
-
-            res = min(whites, res)
+            if r - l + 1 == k:
+                res = min(res, whites)
+                if blocks[l] == "W":
+                    whites -= 1
+                l += 1
 
         return res
+
+
 
 
 
