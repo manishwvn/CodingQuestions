d7a41b1d09a5e9f146784e4efd0daddee2667efe
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-04-27 20:19:13 -0400

    Time: 3 ms (29.92%) | Memory: 17.8 MB (76.88%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,28 +1,13 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
+        j = 0
+        for i in range(len(nums)):
+            if i < len(nums) - 1 and nums[i] == nums[i+1]:
+                nums[i] = nums[i] * 2
+                nums[i+1] = 0
 
-        def double(nums):
-            for i in range(len(nums) - 1):
-                if nums[i] == nums[i+1]:
-                    nums[i] = nums[i] * 2
-                    nums[i+1] = 0
-            return nums
-
-        def shift(nums):
-            j = 0
-            for i in range(len(nums)):
-                if nums[i] != 0:
-                    nums[j] = nums[i]
-                    j += 1
-            
-            while j < len(nums):
-                nums[j] = 0
+            if nums[i]:
+                nums[i], nums[j] = nums[j], nums[i]
                 j += 1
-            return nums
 
-
-
-        return shift(double(nums))
-
-
-        
+        return nums
