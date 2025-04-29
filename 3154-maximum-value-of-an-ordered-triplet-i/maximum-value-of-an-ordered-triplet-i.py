4e36a3649a757b99e0c97e47c801d2c9b94503be
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-04-29 17:23:04 -0400

    Time: 239 ms (5.04%) | Memory: 18 MB (14.59%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
@@ -4,12 +4,10 @@ class Solution:
         res = 0
 
         for i in range(len(nums)):
-            for j in range(i, len(nums)):
-                for k in range(j, len(nums)):
-
-                    if i < j < k:
-                        curr = max((nums[i] - nums[j]) * nums[k], 0)
-                        res = max(curr, res)
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+                    curr = max((nums[i] - nums[j]) * nums[k], 0)
+                    res = max(curr, res)
 
         return res
         
