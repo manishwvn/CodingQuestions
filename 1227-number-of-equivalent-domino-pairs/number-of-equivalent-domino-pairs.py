57e4d186638ba08bdb5c7c3f76a8005b81a58831
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-05-04 14:04:00 -0400

    Time: 11 ms (70.47%) | Memory: 24.5 MB (17.97%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.py
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.py
@@ -4,13 +4,14 @@ class Solution:
         count = 0
 
         for a, b in dominoes:
-            # Step 1: canonicalize
+            
             key = (a, b) if a <= b else (b, a)
 
-            # Step 2: every already‐seen copy of key forms a new pair
-            count += freq.get(key, 0)
-
-            # Step 3: record this domino
-            freq[key] = freq.get(key, 0) + 1
+            if key in freq:
+                count += freq[key]
+                freq[key] += 1
+            else:
+                freq[key] = 1
+            
 
         return count
