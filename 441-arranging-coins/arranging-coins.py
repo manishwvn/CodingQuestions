15102a95f5562f57c70d000279abd7ebb32e5323
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-05-10 16:00:38 -0400

    Time: 544 ms (14.70%) | Memory: 17.7 MB (81.93%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -1,15 +1,12 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
 
-        i = 1
         count = 0
-        while n > 0:
-            if n - i < 0:
-                return count
-            n -= i 
+        i = 1
+        while n >= i:
+            n -= i
             i += 1
             count += 1
-
         return count
 
 
