2b408b9778c04a384a10ebd998b356e2e713ad5d
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-05-10 22:54:20 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (57.35%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -1,12 +1,11 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-
         nums = [int(x) for x in str(num)]
-        max_num = num
+        last = {x: i for i, x in enumerate(nums)}
+        
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):                
-                nums[i], nums[j] = nums[j], nums[i]
-                curr = int(''.join(str(x) for x in nums))
-                max_num = max(max_num, curr)
-                nums[i], nums[j] = nums[j], nums[i]
-        return max_num
+            for d in range(9, nums[i], -1):
+                if last.get(d, -1) > i:
+                    nums[i], nums[last[d]] = nums[last[d]], nums[i]
+                    return int(''.join(map(str, nums)))
+        return num
