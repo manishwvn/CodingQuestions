9ac2e7fe35193dddf7adcb9c91a682645c46793d
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-05-10 22:59:46 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.76%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -1,11 +1,20 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
         nums = [int(x) for x in str(num)]
-        last = {x: i for i, x in enumerate(nums)}
-        
-        for i in range(len(nums)):
-            for d in range(9, nums[i], -1):
-                if last.get(d, -1) > i:
-                    nums[i], nums[last[d]] = nums[last[d]], nums[i]
-                    return int(''.join(map(str, nums)))
-        return num
+        n = len(nums)
+
+        # Track max digit from the right
+        max_idx = n - 1
+        left, right = -1, -1
+
+        for i in range(n - 2, -1, -1):
+            if nums[i] > nums[max_idx]:
+                max_idx = i
+            elif nums[i] < nums[max_idx]:
+                left = i
+                right = max_idx
+
+        if left != -1:
+            nums[left], nums[right] = nums[right], nums[left]
+
+        return int(''.join(str(x) for x in nums))
