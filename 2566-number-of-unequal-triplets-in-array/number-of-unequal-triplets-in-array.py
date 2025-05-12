d42cb46fd78aca5fcac44b3482edd273377d5da6
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-05-12 14:06:08 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (15.70%) - LeetSync

--- 2566-number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.py
+++ 2566-number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.py
@@ -1,13 +1,17 @@
 class Solution:
     def unequalTriplets(self, nums: List[int]) -> int:
-        freq = Counter(nums)
-        unique = list(freq.items())  # (val, count)
-        
+        freq = [0] * 1001  # Since nums[i] in 1 to 1000
+
+        for num in nums:
+            freq[num] += 1
+
         total = 0
         left = 0
         n = len(nums)
 
-        for _, count_mid in unique:
+        for count_mid in freq:
+            if count_mid == 0:
+                continue
             right = n - left - count_mid
             total += left * count_mid * right
             left += count_mid
