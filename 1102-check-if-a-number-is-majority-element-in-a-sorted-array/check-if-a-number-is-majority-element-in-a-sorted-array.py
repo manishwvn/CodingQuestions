71bb6fe9be616ee7364690e1d04f4ff4fee3d205
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-05-14 12:14:39 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (70.76%) - LeetSync

--- 1102-check-if-a-number-is-majority-element-in-a-sorted-array/check-if-a-number-is-majority-element-in-a-sorted-array.py
+++ 1102-check-if-a-number-is-majority-element-in-a-sorted-array/check-if-a-number-is-majority-element-in-a-sorted-array.py
@@ -1,20 +1,39 @@
 class Solution:
     def isMajorityElement(self, nums: List[int], target: int) -> bool:
 
-        n = len(nums)
-        l, r = 0, len(nums) - 1
-        freq = Counter(nums)
+        def find_left(nums, target):
+            l, r = 0, len(nums) - 1
 
-        while l <= r:
-            mid = (l + r) // 2
+            while l <= r:
+                mid = (l + r) // 2
 
-            if nums[mid] == target and freq[nums[mid]] > n // 2:
-                return True
+                if nums[mid] >= target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
 
-            elif nums[mid] < target:
-                l = mid + 1
+            return r
 
-            else:
-                r = mid - 1
+        def find_right(nums, target):
+            l, r = 0, len(nums) - 1
+
+            while l <=r :
+                mid = (l + r) // 2
+
+                if nums[mid] <= target:
+                    l = mid + 1
+
+                else:
+                    r = mid - 1
+            return l
+
+        print(find_left(nums, target))
+        print(find_right(nums, target))
+
+        left = find_left(nums, target) + 1
+        right = find_right(nums, target)
+
+        if right - left > len(nums) // 2:
+            return True
 
         return False
