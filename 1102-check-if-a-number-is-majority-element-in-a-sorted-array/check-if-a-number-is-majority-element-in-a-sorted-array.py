7edfd0d875a1aa7c1e0c1e1a2fe838636b9ba235
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-05-14 12:23:51 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (23.88%) - LeetSync

--- 1102-check-if-a-number-is-majority-element-in-a-sorted-array/check-if-a-number-is-majority-element-in-a-sorted-array.py
+++ 1102-check-if-a-number-is-majority-element-in-a-sorted-array/check-if-a-number-is-majority-element-in-a-sorted-array.py
@@ -12,26 +12,10 @@ class Solution:
 
             return r
 
-        def find_right(l, r, nums, target):
-            while l <=r :
-                mid = (l + r) // 2
-
-                if nums[mid] <= target:
-                    l = mid + 1
-
-                else:
-                    r = mid - 1
-            return l
-
-        print(find_left(0, len(nums) -1, nums, target))
-        print(find_right(0, len(nums) -1, nums, target))
-
         left = find_left(0, len(nums) -1, nums, target) + 1
         right = left + (len(nums) // 2)
+        
         if right < len(nums) and nums[right] == target:
             return True
-
-        # if right - left > len(nums) // 2:
-        #     return True
 
         return False
