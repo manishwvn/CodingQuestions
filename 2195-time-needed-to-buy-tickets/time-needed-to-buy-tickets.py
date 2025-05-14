35aeb6653742ffbc9273dbf802d3ea40695215c8
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-05-14 16:35:06 -0400

    Time: 11 ms (37.06%) | Memory: 17.7 MB (56.44%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,21 +1,17 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
         
-        queue = deque(i for i in range(len(tickets)))
+        if tickets[k] == 1:
+            return k + 1
 
         time = 0
+        while tickets[k] > 0:
+            for i in range(len(tickets)):
+                if tickets[k] == 0:
+                    return time
 
-        while queue:
-            time += 1
-
-            front = queue.popleft()
-            tickets[front] -= 1
-
-            if k == front and tickets[front] == 0:
-                return time
+                if tickets[i] != 0:
+                    tickets[i] -= 1
+                    time += 1
             
-            if tickets[front] != 0:
-                queue.append(front)
-
         return time
-
