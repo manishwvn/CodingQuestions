c65544a9d99dc02d3fb2c230ec11b8d7f961783d
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-05-15 13:07:13 -0400

    Time: 15 ms (82.06%) | Memory: 17.9 MB (52.95%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -6,18 +6,14 @@ class Solution:
         if len(s) == 1:
             return 1
 
-        char_set = set()
+        char_index = {}
         l = 0
         max_len = 0
         for r in range(len(s)):
-
-            #if char is present in set
-            if s[r] in char_set:
-                while s[r] in char_set:
-                    char_set.remove(s[l])
-                    l += 1
-            #else char is not present
-            char_set.add(s[r])
+            #if char is present in hm 
+            if s[r] in char_index and char_index[s[r]] >= l:
+                l = char_index[s[r]] + 1
+            char_index[s[r]] = r
             max_len = max(max_len, r - l + 1)
         
         return max_len
