98ddfbc65da0cbf1a90fd17b36b4dbd394fd0d9a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-05-15 22:50:35 -0400

    Time: 243 ms (43.87%) | Memory: 18.1 MB (86.89%) - LeetSync

---
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
@@ -0,0 +1,26 @@
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+
+        #get total counts of a, b, c:
+        total = [0, 0, 0]
+
+        for char in s:
+            total[ord(char) - ord('a')] += 1
+
+        #check if s is valid
+        if min(total) < k: return -1
+        
+        #sliding window
+
+        res = float('inf')
+        l = 0 
+        for r in range(len(s)):
+            total[ord(s[r]) - ord('a')] -= 1
+
+            while min(total) < k:
+                total[ord(s[l]) - ord('a')] += 1
+                l += 1
+
+            res = min(res, len(s) - (r - l + 1))
+
+        return res
