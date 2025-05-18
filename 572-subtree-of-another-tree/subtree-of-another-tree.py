f4702dc0441b82520cd03424ff66bf26ba82c1a9
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-05-18 19:12:00 -0400

    Time: 4 ms (98.60%) | Memory: 18.2 MB (17.03%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -1,37 +1,10 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        subtree_hashes = set()
 
-        def same_tree(p, q):
-            if not p and not q:
-                return True
-            if not p or not q:
-                return False
-
-            if p.val != q.val:
-                return False
-            
-            return same_tree(p.left, q.left) and same_tree(p.right, q.right)
-
-
-
-        if not root and not subRoot:
-            return True
-
-        if not root or not subRoot:
-            return False
-
-        #case 1 same tree
-        if root.val == subRoot.val:
-            if same_tree(root, subRoot):
-                return True
-
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-
-
-        
+        def serializeNode(node):
+            if not node:
+                return "N"
+            else:
+                return f"({node.val},{serializeNode(node.left)},{serializeNode(node.right)})"
+        return serializeNode(subRoot) in serializeNode(root)
