cafeb2ff8d302122ea0dacc5e278dbff2907d933
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-05-20 14:27:08 -0400

    Time: 60 ms (48.02%) | Memory: 21.1 MB (48.38%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -8,16 +8,38 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        if not root:
-            return None
+        # if not root:
+        #     return None
 
-        if p == root or q == root:
-            return root
+        # if p == root or q == root:
+        #     return root
 
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
+        # elif p.val < root.val and q.val < root.val:
+        #     return self.lowestCommonAncestor(root.left, p, q)
+        # elif p.val > root.val and q.val > root.val:
+        #     return self.lowestCommonAncestor(root.right, p, q)
 
-        if left and right:
-            return root
-        else:
-            return left or right
+        # return root
+
+        def dfs(root):
+            nonlocal lca
+
+            if not root:
+                return
+            
+            lca = root
+            if root == p and root == q:
+                return
+
+            elif p.val < root.val and q.val < root.val:
+                dfs(root.left)
+
+            elif p.val > root.val and q.val > root.val:
+                dfs(root.right)
+            
+            else:
+                return
+
+        lca = root
+        dfs(root)
+        return lca
