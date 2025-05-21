64ff07c371de5ead1aa1f6fd2b4cfff06f69b7ad
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-05-21 14:41:55 -0400

    Time: 10 ms (16.42%) | Memory: 20.5 MB (22.11%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,13 +1,28 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
 
-        counts = Counter(nums)
-        n = len(nums)
+        counts = defaultdict(int)
+
+        for num in nums:
+            counts[num] += 1
+
+            if len(counts) <= 2:
+                continue
+            
+            new_counts = defaultdict(int)
+            for num, count in counts.items():
+
+                if count > 1:
+                    new_counts[num] = count - 1
+            counts = new_counts
+                
 
         res = []
         for num in counts:
-            if counts[num] > n // 3:
+            if nums.count(num) > len(nums) // 3:
                 res.append(num)
-
         return res
+
+
+
         
