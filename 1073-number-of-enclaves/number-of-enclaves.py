bff6e7a4f7b5f43841765639ce417d9109e1aa4d
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-07 14:25:51 -0400

    Time: 67 ms (77.67%) | Memory: 20 MB (86.35%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.py
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -1,31 +1,30 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        visited = set()
         dirs = [(0,1), (1,0), (0,-1), (-1,0)]
 
         def bfs(r, c):
             queue = deque()
             queue.append((r, c))
-            visited.add((r, c))
+            grid[r][c] = 0
 
             while queue:
                 r, c = queue.popleft()
                 for dr, dc in dirs:
                     nr, nc = r + dr, c + dc
-                    if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == 1 and (nr, nc) not in visited:
-                        visited.add((nr, nc))
+                    if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == 1:
+                        grid[nr][nc] = 0
                         queue.append((nr, nc))
 
         for i in range(m):
             for j in range(n):
-                if (i in [0, m - 1] or j in [0, n - 1]) and grid[i][j] == 1 and (i, j) not in visited:
+                if (i == 0 or i == m - 1 or j == 0 or j == n - 1) and grid[i][j] == 1:
                     bfs(i, j)
 
         count = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1 and (i, j) not in visited:
+                if grid[i][j] == 1:
                     count += 1
 
         return count
