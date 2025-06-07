e4cec9764fa735797a1b3074325a3d41fe6cbba7
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-07 17:45:48 -0400

    Time: 6 ms (84.35%) | Memory: 17.7 MB (84.92%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.py
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.py
@@ -3,27 +3,18 @@ from typing import List
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
         stone_sum = sum(stones)
-        n = len(stones)
-        target = stone_sum // 2
+        target = stone_sum // 2  # We aim to get as close to this as possible
 
-        # dp[i][j] = True if subset with sum j is achievable using stones up to index i
-        dp = [[False] * (target + 1) for _ in range(n + 1)]
-        
-        # With 0 stones, subset sum 0 is always possible
-        for i in range(n + 1):
-            dp[i][0] = True
+        dp = [False] * (target + 1)
+        dp[0] = True  # Zero sum is always possible (by taking no stones)
 
-        for i in range(1, n + 1):
-            stone = stones[i - 1]
-            for j in range(target + 1):
-                # If we don't take the current stone
-                dp[i][j] = dp[i - 1][j]
-                
-                # If we take the current stone (and j >= stone)
-                if j >= stone:
-                    dp[i][j] = dp[i][j] or dp[i - 1][j - stone]
+        for stone in stones:
+            for j in range(target, stone - 1, -1):  # iterate backward
+                if dp[j - stone]:
+                    dp[j] = True
 
-        # Find the closest subset sum to target
+        # Find the largest j such that dp[j] is True
         for j in range(target, -1, -1):
-            if dp[n][j]:
+            if dp[j]:
+                # The two subsets are j and stone_sum - j
                 return stone_sum - 2 * j
