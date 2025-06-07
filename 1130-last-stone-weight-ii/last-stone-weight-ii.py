fd727cf89d2273ffe835aa2acbce14658660edca
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-07 17:45:31 -0400

    Time: 10 ms (74.44%) | Memory: 18.2 MB (48.24%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.py
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.py
@@ -1,21 +1,29 @@
+from typing import List
+
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
-
-        def algo(i, total, dp):
-            if total >= target or i == len(stones):
-                return abs(total - (stone_sum - total))
-            
-            if (i, total) in dp:
-                return dp[(i, total)]
-
-            #min(not choose, choose)
-            dp[(i,total)] = min(algo(i + 1, total, dp), algo(i+1, total+stones[i], dp))
-            return dp[(i, total)]
-
-
         stone_sum = sum(stones)
-        target = ceil(stone_sum / 2)
-        dp = {}
-        return algo(0, 0, dp)
+        n = len(stones)
+        target = stone_sum // 2
 
+        # dp[i][j] = True if subset with sum j is achievable using stones up to index i
+        dp = [[False] * (target + 1) for _ in range(n + 1)]
         
+        # With 0 stones, subset sum 0 is always possible
+        for i in range(n + 1):
+            dp[i][0] = True
+
+        for i in range(1, n + 1):
+            stone = stones[i - 1]
+            for j in range(target + 1):
+                # If we don't take the current stone
+                dp[i][j] = dp[i - 1][j]
+                
+                # If we take the current stone (and j >= stone)
+                if j >= stone:
+                    dp[i][j] = dp[i][j] or dp[i - 1][j - stone]
+
+        # Find the closest subset sum to target
+        for j in range(target, -1, -1):
+            if dp[n][j]:
+                return stone_sum - 2 * j
