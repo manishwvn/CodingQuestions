175a75848cce42e889b720ad7a69664b09e7dc56
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-09 20:53:11 -0400

    Time: 31 ms (5.14%) | Memory: 18.3 MB (5.96%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,41 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        #brute force backtracking
+        result = []
+
+        def valid_par(string):
+            count = 0
+
+            for char in string:
+                if char == '(':
+                    count += 1
+                
+                else:
+                    count -= 1
+                
+                if count < 0:
+                    return False
+
+            return count == 0
+
+
+
+        def generate(curr_string):
+            if len(curr_string) == 2 * n:
+                if valid_par(curr_string):
+                    result.append("".join(curr_string))
+                
+                return
+
+            curr_string.append('(')
+            generate(curr_string)
+            curr_string.pop()
+
+            curr_string.append(')')
+            generate(curr_string)
+            curr_string.pop()
+            
+        curr_string = []
+        generate(curr_string)
+        return result
