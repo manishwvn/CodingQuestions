da0397ff1d4af0dd62e81a1b180f2e2da9e22067
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-22 18:20:38 -0400

    Time: 478 ms (90.38%) | Memory: 0B (100.00%) - LeetSync

--- 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
+++ 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
@@ -1,33 +1,30 @@
-with cte as (select
+with borrowed_counts as (
+    select
+        book_id,
+        count(*) as borrowed_copies
+    from
+        borrowing_records
+    where
+        return_date is NULL
+    group by
+        book_id
+)
+
+select
     l.book_id,
     l.title,
     l.author,
     l.genre,
     l.publication_year,
-    l.total_copies,
-    count(b.record_id) as current_borrowers
+    b.borrowed_copies as current_borrowers
 from
-    library_books l
+    library_books l 
 join
-    borrowing_records b
+    borrowed_counts b
 on
     l.book_id = b.book_id
 where
-    return_date is NULL
-group by
-    1,2,3,4,5)
-
-select
-    book_id,
-    title,
-    author,
-    genre,
-    publication_year,
-    current_borrowers
-from
-    cte
-where
-    total_copies - current_borrowers = 0
+    l.total_copies = b.borrowed_copies
 order by
-    current_borrowers desc, title asc;
-
+    current_borrowers DESC,
+    l.title ASC;
