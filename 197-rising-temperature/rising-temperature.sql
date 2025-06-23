1c2ff797e72da5a33ea7448bd3fce2849df40544
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-23 03:36:23 -0400

    Time: 396 ms (85.89%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,18 +1,35 @@
+-- -- select
+-- --     w1.id as Id
+-- -- from
+-- --     Weather w1
+-- -- left join
+-- --     Weather w2
+-- -- on
+-- --     w2.recordDate = w1.recordDate - INTERVAL 1 DAY
+-- -- where
+-- --     w1.temperature > w2.temperature;
+-- with cte as (
 -- select
---     w1.id as Id
+--     *,
+--     LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date,
+--     lag(temperature) over(order by recorddate) as prev
 -- from
---     Weather w1
--- left join
---     Weather w2
--- on
---     w2.recordDate = w1.recordDate - INTERVAL 1 DAY
+--     weather)
+
+-- select
+--     id
+-- from
+--     cte
 -- where
---     w1.temperature > w2.temperature;
+--     temperature > prev
+--     and
+--     recorddate = date_add(prev_date, interval 1 day)
+
 with cte as (
 select
     *,
-    LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date,
-    lag(temperature) over(order by recorddate) as prev
+    lag(recordDate) over(order by recordDate) as prev_date,
+    lag(temperature) over(order by recordDate) as prev_temp
 from
     weather)
 
@@ -21,6 +38,6 @@ select
 from
     cte
 where
-    temperature > prev
+    prev_date = recordDate - interval 1 day
     and
-    recorddate = date_add(prev_date, interval 1 day)
+    temperature > prev_temp;
