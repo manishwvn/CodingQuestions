31f90f05b9d613e3b679cc6991750babcb350c1a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-22 20:13:33 -0400

    Time: 497 ms (84.80%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3898-seasonal-sales-analysis/seasonal-sales-analysis.sql
@@ -0,0 +1,41 @@
+with cte as (
+    select
+        case
+            when month(s.sale_date) in (12, 1, 2) then 'Winter'
+            when month(s.sale_date) in (3, 4, 5) then 'Spring'
+            when month(s.sale_date) in (6, 7, 8) then 'Summer'
+            when month(s.sale_date) in (9, 10, 11) then 'Fall'
+        end as season,
+        p.category,
+        sum(s.quantity) as total_quantity,
+        sum(s.quantity * s.price) as total_revenue
+    from
+        sales s
+    join
+        products p
+    on
+        s.product_id = p.product_id
+    group by
+        season, p.category
+    order by
+        season),
+
+cte2 as (
+    select
+        season,
+        category,
+        total_quantity,
+        total_revenue,
+        dense_rank() over(partition by season order by total_quantity desc, total_revenue desc) rnk
+    from
+        cte)
+
+select
+    season,
+    category,
+    total_quantity,
+    total_revenue
+from
+    cte2
+where
+    rnk = 1
