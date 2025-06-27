f60acdc3c24a5d372d90545cfef257a0408f3282
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-27 12:48:08 -0400

    Time: 465 ms (68.05%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,37 +1,23 @@
-with cte1 as (select
-    *,
-    datediff('2019-08-16', change_date) as diff 
-from
-    products),
-
-cte2 as (select
-    product_id,
-    min(diff) as min_diff
-from
-    cte1
-where
-    diff >= 0
-group by
-    product_id)
+with cte as (
+    select
+        *,
+        dense_rank() over(partition by product_id order by change_date desc) as rnk
+    from
+        products
+    where
+        change_date <= date('2019-08-16')
+)
 
 select
-    product_id,
-    coalesce(sum(price), 10) as price
+    p.product_id,
+    coalesce(c.new_price, 10) as price 
 from
-    (
-select
-    c1.*,
-    c2.min_diff,
-    case
-        when c1.diff = c2.min_diff then new_price
-        when c1.diff <> c2.min_diff then 0
-        else null
-    end as price
-from
-    cte1 c1 
+    products p
 left join
-    cte2 c2 
+    cte c
 on
-    c1.product_id = c2.product_id) as t 
+    p.product_id = c.product_id
+    and
+    c.rnk = 1
 group by
-    product_id
+    p.product_id;
