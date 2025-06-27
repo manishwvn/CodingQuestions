7470724ca305d73f047951f485cfbc5ed2a6eb49
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-27 13:11:42 -0400

    Time: 1115 ms (29.06%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,19 +1,14 @@
 with cte as (select
-    q1.*,
-    sum(q2.weight) as cum_sum
+    *,
+    sum(weight) over(order by turn) as cum_sum
 from
-    queue q1
-left join
-    queue q2
-on
-    q1.turn >= q2.turn
-group by
-    q1.person_id, q1.turn
-having
-    cum_sum <= 1000
-order by
-    cum_sum desc
-limit 1)
+    queue)
 
 select
-    person_name from cte;
+    person_name
+from
+    cte
+where 
+    cum_sum <= 1000
+order by cum_sum desc
+limit 1;
