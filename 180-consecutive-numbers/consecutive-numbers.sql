f469516491e30655a5366842eb931a5d5a5a47e5
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-27 11:22:17 -0400

    Time: 517 ms (85.93%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,23 +1,20 @@
 with cte as (select
-    num,
-    row_number() over(order by id) as rn_id,
-    row_number() over(partition by num order by id) as rn_num
+    *,
+    row_number() over(partition by num order by id) as rnk
 from
     logs),
 
-cte2 as (
-    select
-        num,
-        rn_id - rn_num as grouped
-    from
-        cte
-)
+ cte2 as (select
+    *,
+    id - rnk as grp
+from
+    cte)
 
 select
     distinct num as ConsecutiveNums
 from
     cte2
 group by
-    num, grouped
+    num, grp
 having
-    count(*) >= 3;
+    count(id) >= 3;
