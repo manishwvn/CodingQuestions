d91dde03493fb943f68fcdb3d00f0d64a253581c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-26 20:22:20 -0400

    Time: 707 ms (25.43%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,14 +1,24 @@
-with cte as (select
-    *,
-    dense_rank() over(partition by customer_id order by order_date) as order_rank
-from
-    delivery)
+#beginner approach
+-- find 1st order date
+-- keep only first orders
+-- find immediate %
 
 select
-    round(sum(case
-        when order_date = customer_pref_delivery_date then 1 else 0
-    end) / count(*) * 100, 2) as immediate_percentage
+    round((sum(case
+        when d1.customer_pref_delivery_date = d1.order_date then 1
+        else 0
+    end) / count(*)) * 100, 2) as immediate_percentage
 from
-    cte
-where
-    order_rank = 1
+    delivery d1
+join
+    (select
+        customer_id,
+        min(order_date) as first_order_date
+    from
+        delivery
+    group by
+        customer_id) as d2
+on 
+    d1.customer_id = d2.customer_id
+    and
+    d1.order_date = d2.first_order_date
