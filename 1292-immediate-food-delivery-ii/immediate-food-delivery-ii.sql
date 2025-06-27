f2389f92ac6cde056eb521eb72ece436db978b15
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-26 20:06:14 -0400

    Time: 566 ms (93.66%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,20 +1,14 @@
-WITH RankedOrders AS (
-    SELECT
-        customer_id,
-        order_date,
-        customer_pref_delivery_date,
-        DENSE_RANK() OVER (PARTITION BY customer_id ORDER BY order_date) AS order_rank
-    FROM
-        delivery
-)
-SELECT
-    ROUND(AVG(
-        CASE
-            WHEN order_date = customer_pref_delivery_date THEN 1
-            ELSE 0
-        END
-    ) * 100.00, 2) AS immediate_percentage
-FROM
-    RankedOrders
-WHERE
-    order_rank = 1;
+with cte as (select
+    *,
+    dense_rank() over(partition by customer_id order by order_date) as order_rank
+from
+    delivery)
+
+select
+    round(sum(case
+        when order_date = customer_pref_delivery_date then 1 else 0
+    end) / count(*) * 100, 2) as immediate_percentage
+from
+    cte
+where
+    order_rank = 1
