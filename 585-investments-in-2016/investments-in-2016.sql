5f20d1528bb48488c600af635b49c8599d4f7728
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-28 17:10:49 -0400

    Time: 533 ms (61.47%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,17 +1,32 @@
-select
-    round(sum(tiv_2016), 2) as tiv_2016
-from
-    insurance
-where
-    tiv_2015 in (
-        select
-            tiv_2015
-        from
-            insurance
-        group by
-            tiv_2015
-        having
-            count(pid) > 1
-    )
-and
-    (lat, lon) in (select lat, lon from insurance group by lat, lon having count(pid) = 1);
+WITH SharedTivPolicies AS (
+    -- CTE to get a distinct list of policies that share a tiv_2015 value
+    -- with at least one other policy.
+    SELECT DISTINCT
+        p1.pid
+    FROM
+        Insurance p1
+    JOIN
+        Insurance p2 ON p1.tiv_2015 = p2.tiv_2015 AND p1.pid <> p2.pid
+
+), UniqueLocationPolicies AS (
+    -- CTE to get a list of policies that have a unique geo-location.
+    -- This is done by finding policies that have no "other" partner in the same location.
+    SELECT
+        p1.pid
+    FROM
+        Insurance p1
+    LEFT JOIN
+        Insurance p2 ON p1.lat = p2.lat AND p1.lon = p2.lon AND p1.pid <> p2.pid
+    WHERE
+        p2.pid IS NULL
+)
+-- Final query to sum the tiv_2016 for policies that meet BOTH criteria
+SELECT
+    ROUND(SUM(i.tiv_2016), 2) AS tiv_2016
+FROM
+    Insurance i
+-- Use INNER JOINs to find the intersection of policies that are in both our CTEs
+JOIN
+    SharedTivPolicies stp ON i.pid = stp.pid
+JOIN
+    UniqueLocationPolicies ulp ON i.pid = ulp.pid;
