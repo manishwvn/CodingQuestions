5658ee962ee6643b20b0508800456aca059b0376
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-28 19:46:47 -0400

    Time: 1162 ms (98.87%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,8 +1,12 @@
-SELECT s.name
-FROM salesperson s
-WHERE s.sales_id NOT IN (
-    SELECT o.sales_id
-    FROM orders o
-    JOIN company c ON o.com_id = c.com_id
-    WHERE c.name = 'RED'
-);
+SELECT
+    s.name
+FROM
+    salesperson s
+LEFT JOIN
+    orders o ON s.sales_id = o.sales_id
+LEFT JOIN
+    company c ON o.com_id = c.com_id AND c.name = 'RED'
+group by
+    s.sales_id, s.name
+HAVING
+    COUNT(c.com_id) = 0;
