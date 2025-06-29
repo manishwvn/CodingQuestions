5fc8f4e9a213febf14da25341311f7053184bc02
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-29 18:53:43 -0400

    Time: 1954 ms (96.89%) | Memory: 0B (100.00%) - LeetSync

--- 1735-the-most-frequently-ordered-products-for-each-customer/the-most-frequently-ordered-products-for-each-customer.sql
+++ 1735-the-most-frequently-ordered-products-for-each-customer/the-most-frequently-ordered-products-for-each-customer.sql
@@ -1,56 +1,22 @@
--- with order_counts as(
---     select
---         customer_id,
---         product_id,
---         count(*) as counts
---     from
---         orders o
---     group by
---         1,2
---     ),
-
--- order_freqs as (
---     select
---         *,
---         dense_rank() over(partition by customer_id order by counts desc) as freqs
---     from
---         order_counts
---     )
-
--- select 
---     f.customer_id,
---     f.product_id,
---     p.product_name
--- from
---     order_freqs f
--- join
---     products p
--- on
---     f.product_id = p.product_id
--- where 
---     freqs = 1;
-
-
-with order_freqs as(
-    select
-        customer_id, 
-        product_id,
-        dense_rank() over(partition by customer_id order by count(product_id) desc) as most_frequent
-    from
-        orders
-    group by
-        customer_id, product_id
-)
+with cte as (
+select
+    o.customer_id,
+    o.product_id,
+    p.product_name,
+    dense_rank() over(partition by customer_id order by count(o.product_id) desc) as rnk
+from
+    orders o 
+join
+    products p 
+on
+    o.product_id = p.product_id
+group by
+    o.customer_id, o.product_id)
 
 select
-    f.customer_id,
-    f.product_id,
-    p.product_name
+    customer_id,
+    product_id,
+    product_name
 from
-    order_freqs f
-join
-    products p
-on
-    f.product_id = p.product_id
-where
-    most_frequent = 1;
+    cte
+where rnk = 1;
