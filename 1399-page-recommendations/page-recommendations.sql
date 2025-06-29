c5d5de6025711ef53de267e145ed5dc6fda37e94
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-29 16:50:56 -0400

    Time: 534 ms (75.73%) | Memory: 0B (100.00%) - LeetSync

--- 1399-page-recommendations/page-recommendations.sql
+++ 1399-page-recommendations/page-recommendations.sql
@@ -1,24 +1,16 @@
-with cte as (
-    select
-        *
-    from
-        friendship
-    where
-        user1_id = 1
-        or
-        user2_id = 1
-)
-
 select
-    distinct l.page_id as recommended_page
+    distinct page_id as recommended_page
 from
-    likes l 
+    friendship f
 join
-    cte c 
+    likes l
 on
-    l.user_id = c.user1_id
+    (l.user_id = f.user2_id
+    and
+    f.user1_id = 1)
     or
-    l.user_id = c.user2_id
+    (l.user_id = f.user1_id
+    and
+    f.user2_id = 1)
 where
     l.page_id not in (select distinct page_id from likes where user_id = 1)
-    
