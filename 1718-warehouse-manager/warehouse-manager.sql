f13c0ee2c937c54a457943cf9d434bc2234d42b4
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-28 23:44:12 -0400

    Time: 586 ms (69.00%) | Memory: 0B (100.00%) - LeetSync

--- 1718-warehouse-manager/warehouse-manager.sql
+++ 1718-warehouse-manager/warehouse-manager.sql
@@ -1,11 +1,11 @@
 select
     w.name as warehouse_name,
-    sum(p.width * p.height * p.length * w.units) as volume
+    sum(p.width * p.length * p.height * w.units) as volume
 from
-    warehouse w
-join
-    products p
+    warehouse w 
+left join
+    products p 
 on
-    p.product_id = w.product_id
+    w.product_id = p.product_id
 group by
     w.name
