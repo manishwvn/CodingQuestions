39bc95827a5ab27dc01e4cc1f88021705983b405
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-29 15:59:00 -0400

    Time: 1353 ms (82.81%) | Memory: 0B (100.00%) - LeetSync

--- 1932-grand-slam-titles/grand-slam-titles.sql
+++ 1932-grand-slam-titles/grand-slam-titles.sql
@@ -1,22 +1,20 @@
-select p_id as player_id, player_name, sum(wins) as  grand_slams_count from 
+with cte as (select
+    p.player_id, p.player_name, sum(c.wimbledon = p.player_id) as wim_wins,
+    sum(c.fr_open = p.player_id) as fr_wins,
+    sum(c.us_open = p.player_id) as us_wins,
+    sum(c.au_open = p.player_id) as au_wins
+from
+    players p 
+cross join
+    championships as c
+group by
+    1,2)
 
-(select wimbledon as p_id,count(*) as wins from Championships
-group by 1
-
-union all 
-
-select Fr_open as p_id,count(*) as wins from Championships
-group by 1
-
-union all
-
-select US_open as p_id,count(*) as wins from Championships
-group by 1
-
-union all
-
-select Au_open as p_id,count(*) as wins from Championships
-group by 1
-) AS X 
-inner join Players Y  on X.p_id = Y.player_id
-group by 1,2
+select
+    player_id,
+    player_name,
+    (wim_wins + fr_wins + us_wins + au_wins) as grand_slams_count
+from
+    cte
+where
+    wim_wins + fr_wins + us_wins + au_wins > 0;
