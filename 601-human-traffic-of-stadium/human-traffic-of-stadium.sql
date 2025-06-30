6ba1b359902cd120fed42658dbb7e5f0f1649778
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-30 12:00:24 -0400

    Time: 357 ms (75.03%) | Memory: 0B (100.00%) - LeetSync

--- 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -1,30 +1,20 @@
-WITH StadiumGroups AS (
-    -- Step 1: Filter for valid days and create the grouping key 'diff'
-    SELECT
-        id,
-        visit_date,
-        people,
-        -- This identifies each group of consecutive days
-        id - ROW_NUMBER() OVER(ORDER BY id) AS diff
-    FROM
-        Stadium
-    WHERE
-        people >= 100
-)
--- Step 2 & 3: Count the size of each group and filter
-SELECT
+WITH res_cte AS (
+SELECT 
+    *,
+    id - row_number() OVER (ORDER BY visit_date) level
+FROM
+    Stadium
+WHERE people >= 100)
+,final_cte AS (
+SELECT 
     id,
     visit_date,
-    people
-FROM (
-    -- Add a count of how many rows are in each 'diff' group
-    SELECT
-        *,
-        COUNT(*) OVER(PARTITION BY diff) AS consecutive_days
-    FROM
-        StadiumGroups
-) AS FinalCounts
-WHERE
-    consecutive_days >= 3
-ORDER BY
-    visit_date;
+    people,
+    level,
+    COUNT(*) OVER (PARTITION BY level) consec_count
+    FROM res_cte
+)
+
+SELECT id, visit_date, people FROM final_cte 
+WHERE consec_count > 2 ORDER BY visit_date;
+
