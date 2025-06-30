7abd967f6a210f2c8bd5585a165645b4b9834002
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-30 01:13:24 -0400

    Time: 460 ms (67.32%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2208-account-balance/account-balance.sql
@@ -0,0 +1,8 @@
+SELECT 
+    account_id, 
+    day, 
+    sum(case when type = 'Deposit' then amount else -amount end) 
+    over(partition by account_id order by day asc)     
+    as balance 
+FROM 
+    transactions
