255f4c771698c33394971399061389922850b28a
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-30 03:13:32 -0400

    Time: 735 ms (80.11%) | Memory: 0B (100.00%) - LeetSync

--- 1914-find-the-subtasks-that-did-not-execute/find-the-subtasks-that-did-not-execute.sql
+++ 1914-find-the-subtasks-that-did-not-execute/find-the-subtasks-that-did-not-execute.sql
@@ -1,16 +1,42 @@
-with recursive cte as (
-    select task_id, subtasks_count from tasks
-    union
-    select task_id, subtasks_count - 1 from cte where subtasks_count > 1
+--  for each task there is a subtasks (subtask 1, 2, 3 ... 20) such that each task has subtask count (2 ... 20)
 
+--  from eg.
+--  for task = 1, subtask count = 3 and subtask = 2 executed -> 1, 3 not executed so need to display these
+
+-- key step -> convert tasks table in the form of (task_id, subtask_id) based on the number of counts.
+
+with recursive cte as(
+    select
+        task_id,
+        1 as subtask_id
+    from
+        tasks
+    union all
+    select
+        c.task_id,
+        c.subtask_id + 1
+    from
+        cte c
+    join
+        tasks t
+    on
+        c.task_id = t.task_id
+    where
+        c.subtask_id < t.subtasks_count
 )
 
 select
-    task_id,
-    subtasks_count as subtask_id
+    c.task_id,
+    c.subtask_id
 from
-    cte
+    cte c 
+left join
+    executed e
+on
+    c.task_id = e.task_id
+    and
+    c.subtask_id = e.subtask_id
 where
-    (task_id, subtasks_count) not in (
-        select * from executed
-    )
+    e.subtask_id is null
+order by
+    1, 2
