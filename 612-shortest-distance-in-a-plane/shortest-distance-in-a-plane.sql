89d94dca186ea9d260d5527824b8ea1cf50c2c80
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-30 12:56:00 -0400

    Time: 231 ms (73.63%) | Memory: 0B (100.00%) - LeetSync

--- 612-shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
+++ 612-shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
@@ -9,7 +9,8 @@ with distinct_points as(
     join
         point2d p2
     where
-        (p1.x, p1.y) != (p2.x, p2.y))
+        p1.x < p2.x OR (p1.x = p2.x AND p1.y < p2.y)
+)
 
 select
     round(min(sqrt(pow((x1-x2), 2) + pow((y1-y2),2))), 2) as shortest
