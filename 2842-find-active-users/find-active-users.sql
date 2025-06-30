f37085fea107de1bfb58eb6db389cae388345a90
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-30 15:27:45 -0400

    Time: 871 ms (77.88%) | Memory: 0B (100.00%) - LeetSync

--- 2842-find-active-users/find-active-users.sql
+++ 2842-find-active-users/find-active-users.sql
@@ -1,13 +1,9 @@
 with cte as (
-select
-    *,
-    lead(created_at) over(partition by user_id order by created_at) as next_date
-from
-    users)
+    select
+        *,
+        count(user_id) over(partition by user_id order by created_at range between interval 7 day preceding and current row) as roll_cnt
+    from
+        users
+)
 
-select
-    distinct user_id
-from
-    cte
-where
-    datediff(next_date, created_at) <= 7
+select distinct user_id from cte where roll_cnt > 1;
