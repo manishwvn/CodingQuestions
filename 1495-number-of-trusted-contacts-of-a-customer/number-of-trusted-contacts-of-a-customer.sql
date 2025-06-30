17e70f50f176d8c4934decfff2cb2d53e7fe35a8
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-30 12:15:38 -0400

    Time: 1421 ms (35.61%) | Memory: 0B (100.00%) - LeetSync

--- 1495-number-of-trusted-contacts-of-a-customer/number-of-trusted-contacts-of-a-customer.sql
+++ 1495-number-of-trusted-contacts-of-a-customer/number-of-trusted-contacts-of-a-customer.sql
@@ -1,27 +1,13 @@
-select
-    i.invoice_id,
-    c1.customer_name,
-    i.price,
-    count(c2.contact_name) as contacts_cnt,
-    count(c3.customer_name) as trusted_contacts_cnt
-from
-    invoices i
-left join
-    customers c1
-on
-    i.user_id = c1.customer_id
-left join
-    contacts c2
-on
-    i.user_id = c2.user_id
-left join
-    customers c3
-on
-    c2.contact_name = c3.customer_name
-    and
-    c2.contact_email = c3.email
-group by
-    1,2,3
-order by
-    1;
-
+# Write your MySQL query statement below
+SELECT 
+    i.invoice_id, 
+    c.customer_name, 
+    i.price, 
+    COUNT(co.contact_name) contacts_cnt,
+    COUNT(c2.email) trusted_contacts_cnt
+FROM Invoices i
+JOIN Customers c ON i.user_id = c.customer_id
+LEFT JOIN Contacts co ON c.customer_id = co.user_id
+LEFT JOIN Customers c2 ON co.contact_email = c2.email
+GROUP BY 1
+ORDER BY 1
