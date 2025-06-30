34a869034e03a8a501d939c8ad93637557aa064c
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-30 17:20:41 -0400

    Time: 1054 ms (5.17%) | Memory: 0B (100.00%) - LeetSync

--- 1328-monthly-transactions-ii/monthly-transactions-ii.sql
+++ 1328-monthly-transactions-ii/monthly-transactions-ii.sql
@@ -1,58 +1,39 @@
-with approvals as (
-select
-    date_format(t.trans_date, '%Y-%m') as month,
-    t.country as country,
-    sum(case when t.state = 'approved' then 1 else 0 end) as approved_count,
-    sum(case when t.state = 'approved' then amount else 0 end) as approved_amount
-from
-    transactions t
-group by
-    month, country),
-
-charges as (
-    select
-        date_format(c.trans_date, '%Y-%m') as month,
-        t.country,
-        count(*) as chargeback_count,
-        sum(t.amount) as chargeback_amount
-    from
-        chargebacks c
-    left join
-        transactions t
-    on
-        c.trans_id = t.id
-    group by
-        month, country
+with chargebacks_cte as (
+    select 
+        DATE_FORMAT(chargebacks.trans_date, '%Y-%m') AS month,
+        country,
+        'back' as state, 
+        amount 
+    from 
+        chargebacks 
+    join 
+        transactions 
+    on 
+        chargebacks.trans_id = transactions.id 
 ),
-
-result as (
-    select
-        a.month,
-        a.country,
-        a.approved_count,
-        a.approved_amount,
-        ifnull(c.chargeback_count, 0) as chargeback_count,
-        ifnull(c.chargeback_amount, 0) as chargeback_amount
-    from
-        approvals a
-    left join
-        charges c
-    on
-        a.month = c.month and a.country = c.country
-    union
-    select
-        c.month,
-        c.country,
-        ifnull(a.approved_count, 0) as approved_count,
-        ifnull(a.approved_amount, 0) as approved_amount,
-        ifnull(c.chargeback_count, 0) as chargeback_count,
-        ifnull(c.chargeback_amount, 0) as chargeback_amount
-    from
-        approvals a
-    right join
-        charges c
-    on
-        a.month = c.month and a.country = c.country)
-
-select * from result
-    where approved_count+approved_amount+chargeback_count+chargeback_amount <> 0;
+transactions_cte as (
+    select 
+        DATE_FORMAT(trans_date, '%Y-%m') AS month,
+        country,
+        'approved' as state, 
+        amount 
+    from 
+        transactions
+    where 
+        state = 'approved'
+)
+select 
+    month, 
+    country, 
+    sum(case when state = 'approved' then 1 else 0 end) as approved_count, 
+    sum(case when state = 'approved' then amount else 0 end) as approved_amount, 
+    sum(case when state = 'back' then 1 else 0 end) as chargeback_count, 
+    sum(case when state = 'back' then amount else 0 end) as chargeback_amount
+from 
+    (
+        select month, country, state, amount from chargebacks_cte
+        union all 
+        select month, country, state, amount from transactions_cte
+    ) tmp
+group by 
+    1, 2
