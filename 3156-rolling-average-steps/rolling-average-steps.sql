c59d2192ad3d9d5b24ffcb67ecc1669a534c2efe
Manish Wunnava <48592499+manishwvn@users.noreply.github.com>
2025-06-30 16:43:07 -0400

    Time: 279 ms (81.82%) | Memory: 0B (100.00%) - LeetSync

--- 3156-rolling-average-steps/rolling-average-steps.sql
+++ 3156-rolling-average-steps/rolling-average-steps.sql
@@ -1,12 +1,12 @@
-SELECT
-    t1.user_id,
-    t1.steps_date,
-    ROUND((t1.steps_count + t2.steps_count + t3.steps_count) / 3.0, 2) AS rolling_average
-FROM
-    Steps t1
-JOIN
-    Steps t2 ON t1.user_id = t2.user_id AND t1.steps_date = t2.steps_date + INTERVAL '1' DAY
-JOIN
-    Steps t3 ON t1.user_id = t3.user_id AND t2.steps_date = t3.steps_date + INTERVAL '1' DAY
-ORDER BY
-    t1.user_id, t1.steps_date;
+
+WITH CTE AS
+(
+SELECT user_id, steps_date,
+ROUND(AVG(steps_count) OVER(PARTITION BY user_id ORDER BY steps_date ROWS BETWEEN 2 PRECEDING AND CURRENT ROW),2) AS rolling_average,
+LAG(steps_date,2) OVER(PARTITION BY user_id ORDER BY steps_date) AS lag_date
+FROM Steps
+)
+SELECT user_id,steps_date, rolling_average
+FROM CTE
+WHERE DATEDIFF(steps_date, lag_date) = 2
+ORDER BY 1,2
